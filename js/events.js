0f7ee5b83c364b525aafc7a479b87063c0c4a768
Menachem Abraham <menachemabraham@gmail.com>
2016-09-09 04:41:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!");
+  })
+}
+
+//'load' event will not be called after ready() is run (will have already loaded)
+function frameIt() {
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(e) {
+    if (71 === e.which) {
+      alert('the -g- key has been compromised!!!');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
