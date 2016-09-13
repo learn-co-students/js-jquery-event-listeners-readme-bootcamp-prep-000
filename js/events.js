b4fd00bd539e8d85a202782b66a7da618e4cef31
Yue Wang <yue.bruce.wang@gmail.com>
2016-09-13 23:07:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
 //define functions here
+function getIt () {
+  $("p").on("click", function () {
+    alert("Hey!");
+  });
+}
+
+function frameIt () {
+  $("img").on("load", function () {
+    $("img").addClass("tasty");
+  });
+}
+
+function submitIt () {
+  $("form").on("submit", function () {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt () {
+  $("form input").on("keydown", function (g) {
+    if (g.which === 71) {
+      alert("You pressed 'g'");
+    }
+  });
+}
+
 
 $(document).ready(function(){
 
