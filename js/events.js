80b04f1d7cf9dc9f5df3f9d1219c59b6ba6d54c0
Jon Fougner <jon.fougner@gmail.com>
2016-09-13 20:38:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $("input").on("keydown", function(key) { // also works for form rather than input, but the former seems overbroad
+    if (key.which == 71) {
+      alert('g was pressed');
+      return;
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.")
+    })
+}
+
 $(document).ready(function(){
 
-// call functions here
+// call your functions here
+
+$getIt();
+$frameIt();
+$pressIt();
+$submitIt();
 
 });
