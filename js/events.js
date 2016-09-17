af03d43c9a959a4495c7f11605c4c30a2c1ca353
William Barela <william.barela.dev@gmail.com>
2016-09-17 14:25:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!');
+  })
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown',function(key){
+    if(key.which === 71){
+      alert('The letter "g" has been pressed!');
+    }
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 
 });
