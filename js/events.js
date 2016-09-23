893a0fa5a4c931f888c19a06b446a42f4022183c
Jonathan McAlpine <j.g.mcalpine@gmail.com>
2016-09-23 17:31:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert('You have pressed the g key');
+      return;
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
 
 });
