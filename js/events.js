f7c3d0295bc2108d0923468eb5de8f25108c6f18
Pimpila Howe <pimpila@gmail.com>
2016-09-27 00:37:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('g has been pressed');
+    }
+  })
+}
+
+function submitIt() {
+  alert('Your form is going to be submitted now.');
+}
 
 $(document).ready(function(){
 
 // call functions here
 
+$('p').on('click', getIt());
+$('img').on('load', frameIt());
+$('#typing').on('keydown', pressIt());
+$('input:last').on('submit', submitIt());
 });
