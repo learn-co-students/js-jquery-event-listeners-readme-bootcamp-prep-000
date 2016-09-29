a7bf4acd39ffedb318a2a5d383be25597b7270fe
Alexander Boring <alexboring@hotmail.com>
2016-09-29 22:22:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  });
+}
+function pressIt() {
+  $(document).on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('g has been pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+$(document).ready(function() {
+ getIt()
+ frameIt()
+ pressIt()
+ submitIt()
+})
+
 
 // call functions here
-
-});
