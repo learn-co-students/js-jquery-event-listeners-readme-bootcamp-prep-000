09dbf1965269d93c27dd5a33cbff5c3357149d52
Patrick Townley <patricktownley@gmail.com>
2016-10-10 13:09:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+
+function frameIt(){
+  $('img').on('load', function(){
+      $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+    if(e.which === 71){
+      alert("You done pressed g!");
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
