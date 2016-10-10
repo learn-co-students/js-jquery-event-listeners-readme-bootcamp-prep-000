b311ce35f39da673bd310c061bda22cb20993a98
Matthew Palumbo <mmpal78@yahoo.com>
2016-10-10 15:39:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
+var getIt = function() {
+    alert('Hey!')
+
+}
+
+
+function frameIt() {
+
+      $('img').attr('class', 'tasty');
+
+}
+
+
+var pressIt = function(e) {
+  const key = parseInt(e.detail || e.which)
+  if(key === 71) {
+    alert('You pressed the G key')
+  }
+}
+
+
+var submitIt = function() {
+  alert('Your form is going to be submitted now.')
+}
+
+
+
 
 $(document).ready(function(){
+  $('p').on('click', getIt);
+
+$('img').on('load', frameIt);
+
+$('input').on('keydown', pressIt);
+
+$('form').submit('submitIt');
 
 // call functions here
+//$('input').addEventListener('click', func)
 
 });
