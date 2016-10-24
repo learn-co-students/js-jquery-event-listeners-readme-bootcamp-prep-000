1567671aec1c44c8f342174ef2cc0b5114db020d
Caroline Sheedy <carolinesheedy28@gmail.com>
2016-10-24 12:00:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('you have pressed the letter G')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+    return;
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
