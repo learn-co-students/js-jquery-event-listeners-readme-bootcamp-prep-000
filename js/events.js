87d435f027014e27a90481a5f37c4c2811f0b25d
Ethan Ryan <ethanryan@gmail.com>
2016-10-27 21:13:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,60 @@
+// INSTRUCTIONS
+// You will write your code in js/events.js.
+// You will want to define your functions outside of the document ready,
+// and call them inside of this. This way, your tests will run as expected
+// and you can test your code in the browser as well.
+//
+
+
+// Define a function getIt that does not accept a parameter.
+// The function should bind a click event to the p tag.
+// When the paragraph is clicked, the function should alert "Hey!".
+//
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  })
+}
+
+// Define a function frameIt that does not accept a parameter.
+// The function should bind the load event that adds the class tasty
+// to the image to add a red frame to the image.
+//
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  })
+}
+// Define a function pressIt that does not accept a parameter.
+// The function should bind a keydown event to the input field
+// of the form that alerts a user when they have pressed the g key.
+//
+function pressIt() {
+  $("input#typing").on("keydown", function(key) {
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+// Define a function submitIt that does not accept a parameter.
+// The function should bind a submit event to the form
+// that alerts "Your form is going to be submitted now.".
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
+
 //define functions here
 
+
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
