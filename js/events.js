65117aa1b5f57a11bb234f3c208647c907e3cec9
Joe Browm <jaybrizle@gmail.com>
2016-10-28 07:23:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $(document).on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e){
+    if(e.which === 71){
+      alert('alert')
+    }
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt()
+  frameIt()
+  submit()
+  //pressIt()
 });
