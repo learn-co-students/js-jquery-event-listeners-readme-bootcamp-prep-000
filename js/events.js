7f934f123f78200d2023f4f9614d2a3fd6a41a68
Earth Water <earthwater13@gmail.com>
2016-11-02 02:38:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,29 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').click(function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g is pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
