2ade57dc0f971b82ce7ca4b72e2817b7390b2b52
Daniel Lempesis <danlrcn@hotmail.com>
2016-11-05 18:58:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,46 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+  //actions you want to happen
+});
+}
+
+function pressIt(){
+  $('form').on('keydown', function(keyPress){
+    if(keyPress.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $("img").addClass("tasty");
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+ return getIt()
 
+ return pressIt()
+
+ return submitIt()
+
+ return frameIt()
 });
