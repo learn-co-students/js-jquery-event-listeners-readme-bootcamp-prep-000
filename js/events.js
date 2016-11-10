bf613712eb55c12b43aaeb73c45c0dbfdaedacf4
Yi-Xuan Lau <yixuan.lau@gmail.com>
2016-11-10 10:29:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
+function getIt(){
+  $('p').on('click', alert("Hey!"));
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if (key.detail === 71 || key.which === 71 ){
+      alert("You have pressed the g key!");
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', alert('Your form is going to be submitted now.'));
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
