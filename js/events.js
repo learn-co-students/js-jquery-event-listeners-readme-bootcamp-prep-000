0a7f82f1a9dbb4334a70e523163ab443e51bd65f
Josef Butts <jbutts@utexas.edu>
2016-11-11 02:36:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
-$(document).ready(function(){
 
 // call functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+    //action you want taken
 });
+}
+/*
+function frameIt(){
+  $('img)'.on('load', function(){
+    $('img').addClass("tasty");
+  });//close
+*/
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $('#typing').on("keydown", function(key){
+    if(key.which == 71){
+      alert('s was pressed');
+  }
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
