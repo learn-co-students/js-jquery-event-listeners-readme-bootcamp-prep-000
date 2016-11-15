6ee640db685fc103c111841d7d8b18bec9581897
Brittany Hill <brit.hill10@gmail.com>
2016-11-15 20:38:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -14,7 +14,6 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keydown', function(e) {
-    // 'g' corresponds to 71
     if (e.which === 71) {
       alert("You pressed 'g'!")
     }
