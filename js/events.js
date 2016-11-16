abd9413ab2cc68b3760e36b1168497599d6a7144
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-11-16 02:13:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    if(key === 71) alert('You have pressed g button!');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
 });
