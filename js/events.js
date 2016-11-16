a28d8500ec112b4dfa85bcb9b38bc3ffcc35d60f
Laamia Islam <laamia.islam@gmail.com>
2016-11-16 05:57:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  alert("Hey!");
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  $('#typing').on("keydown",function(key){
+    if(key.which === 71) {
+      alert("You have pressed the g!");
+    }
+  });
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.");
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').on("click",getIt());
+  $('img').on("load",frameIt());
+  $('#typing').on("keydown",pressIt(key));
+  $('form').on("submit",submitIt());
 });
