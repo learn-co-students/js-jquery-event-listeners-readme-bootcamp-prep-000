278287952d3da53ae60feacbf72aaad8b9805ba7
Pak Belalang <opblang@yandex.com>
2016-11-30 14:52:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,10 +6,13 @@ function getIt() {
 }
 
 function frameIt() {
-  
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
 }
 
 $(document).ready(function(){
 // call functions here
   getIt()
+  frameIt()
 });
