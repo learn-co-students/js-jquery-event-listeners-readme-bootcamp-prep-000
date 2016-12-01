fa1124d8fee41163f20970395c2c626ddf0dcb68
Michael Capps <michaelcapps1772@yahoo.com>
2016-12-01 02:04:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+};
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  });
+};
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+};
+
+function pressIt(){
+  $('#typing').on('keydown', function(k){
+    if (k.which == 71){
+      alert("'g' key was pressed!!!");
+    }
+  })
+};
