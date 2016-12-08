8fbbc6af4087d2a676ae2e5d57d392f8ddd2642a
Kevin Adkins <kevinladkins@gmail.com>
2016-12-08 19:59:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,45 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $("p").on("click", function(e) {
+  alert("Hey!");
+});
+}
 
+function pressIt() {
+  $("input").on("keydown", function(e){
+    if (e.which == 71) {
+      alert("You have pressed g");
+    }
+  });
+}
+
+
+/* function pressIt(key) {
+  if (key.which == 71) {
+    alert("You have pressed g");
+    return;
+  }
+  return;
+} */
+
+
+function frameIt() {
+  $("img").addClass("tasty");
+  $("img").css("border-color : red");
+  return;
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.");
+  return;
+}
+
+$(document).ready(function(){
+  getIt();
+  pressIt();
+  $("form").on("submit", submitIt());
+  $("img").on("load", frameIt());
 // call functions here
 
 });
