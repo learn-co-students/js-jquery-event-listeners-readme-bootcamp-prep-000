276b645db0aed151cb6577c8f7da7de812b4b27e
Eric Schwertfeger <ericsgeek@gmail.com>
2016-12-18 07:49:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').click(function() { alert('Hey!');});
+}
+
+function frameIt() {
+  $('img').on('load', function() { $('img').addClass('tasty');});
+}
+
+function pressIt() {
+  $('input').keydown(function(e) {
+    const key = parseInt(e.detail || e.which);
+    console.warn(`got key: ${key}`);
+    if(key === 71) { alert("You just pressed the 'g' key.");}
+  });
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
