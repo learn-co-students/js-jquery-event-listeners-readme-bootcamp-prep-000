790716a7caf38ffea5ef7c4384fe5673eb207ebc
Scott Plunkett <scottyplunkett@gmail.com>
2016-12-28 18:10:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,27 @@
+function getIt(){
+    $('p').on("click", function() {
+        alert("Hey!")
+    })
+};
+function frameIt() {
+    $('img').on('load', function(){
+        $('img').addClass('tasty')
+    })
+};
+function pressIt(){
+    $(":input").on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+})
+};
+function submitIt(){
+    $('form').on('submit', function() {
+        alert("Your form is going to be submitted now.")
+    })
+}
+
+
 //define functions here
 
 $(document).ready(function(){
