f675bf397ba7ae7114e6e9b6a116251c51efa8f1
Vasily Drovetskiy <drovv2011@gmail.com>
2016-12-30 00:38:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,26 @@
+function getIt() {
+  $('p').on('click', function(event){
+    alert("Hey!")
+  })
+}
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty")
+  })
+}
+function pressIt() {
+  $('input').on('keydown', function(element) {
+    console.log(element.which)
+    if (element.which == 71){
+      alert('"g" is pressed')
+    }
+  })
+}
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 //define functions here
 
 $(document).ready(function(){
