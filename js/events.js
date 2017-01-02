2d21b3de84259e346f11ee25e60ac59107a1c995
Ryan Christoffersen <ryanelliotchristo@gmail.com>
2017-01-02 19:51:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
-// call functions here
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(event) {
+    if (event.which === 71) {
+      alert();
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+$(document).ready(function() {
+
+  // call functions here
+
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
