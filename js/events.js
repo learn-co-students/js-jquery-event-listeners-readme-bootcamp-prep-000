9054fe2f65d808eb62563b22c07c54a14496280d
Jack Mansager <mansagerj@mac.com>
2017-01-11 04:51:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $(document).on('load', function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if (e.which == 71) {
+      alert('You pressed the g key!');
+    }
+  })
+}
+
+
+
+$(document).ready(function() {
 
 // call functions here
+getIt();
+frameIt();
+submitIt();
+pressIt();
 
 });
