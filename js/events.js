d1a1cc1c397c6453b75d04153445b80051940a6c
Shelly Xu <xushell@gmail.com>
2017-01-16 03:43:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+
 
 $(document).ready(function(){
-
-// call functions here
-
 });
+
+//define functions here
+function getIt(){
+  $("p").on("click",function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt(){
+  $("img").on("load",function(){
+    $("img").addClass("tasty");
+    //$(this).css("border","1px solid black");
+  })
+}
+
+function pressIt(){
+  $("form").on("keydown", function(key){
+    if(key.which == 71){
+      alert("You have pressed g!");
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert ("Your form is going to be submitted now.");
+  })
+}
