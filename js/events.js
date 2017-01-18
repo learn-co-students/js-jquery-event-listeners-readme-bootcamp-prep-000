312d6ac52b2f53e8b7769f44f97bd47dd67c6c47
Caroline Wynspeare <carolinewynspeare@gmail.com>
2017-01-18 22:38:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
+function getIt () {
+  $("p").on("click", function(){
+  alert("Hey!");
+}
+)};
+
+function frameIt(){
+  $("img").on("load", function(){
+$("img").addClass( " tasty" );
+})
+}
+
+function pressIt(){
+  $("input").on("keydown", function() {
+    if (key.which == 103){
+      alert('g was pressed');
+  })
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
 
 });
