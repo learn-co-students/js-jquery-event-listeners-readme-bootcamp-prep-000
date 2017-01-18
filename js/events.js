a8bbd1afaa5ac00abf4e9995d860f366ae7e5db1
Caroline Wynspeare <carolinewynspeare@gmail.com>
2017-01-18 23:39:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -8,16 +8,22 @@ function getIt () {
 function frameIt(){
   $("img").on("load", function(){
 $("img").addClass( " tasty" );
-})
 }
+)};
 
 function pressIt(){
-  $("input").on("keydown", function() {
-    if (key.which == 103){
+  $("form").on("keydown", function(key) {
+    if (key.which == 71){
       alert('g was pressed');
-  })
-}
+  }
+});
+};
 
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  }
+)};
 
 $(document).ready(function(){
 
