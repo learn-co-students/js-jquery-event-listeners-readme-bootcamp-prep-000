0ac11d6e5cb8774e79391c9182959dd8be6dfa86
Amjad Hussain <amjad.hussain544@gmail.com>
2017-01-22 16:40:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,33 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(e){
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(e){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+    if(key === 71){
+      alert("You pressed g!");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(e){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+
 $(document).ready(function(){
 
 // call functions here
