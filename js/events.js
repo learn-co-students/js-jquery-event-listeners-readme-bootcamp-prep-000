91ab4efc4e35f3a40ed742ca809becd5c1e1a59d
Elise Yohm <elisery@hotmail.com>
2017-02-04 01:19:35 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+    $('p').on('click', function() {
+        alert("Hey!")
+    })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+    $(document).on('keydown', function(key) {
+      if (key.which == 71) {
+        alert("Gee, why did you do that?")
+      }
+    })
+}
+
+function submitIt() {
+  $(document).on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
