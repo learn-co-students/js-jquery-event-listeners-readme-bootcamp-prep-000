c9f8f2e47c210907cd6bd8a6dde319933693e7f8
Drew Hart <drew@drewhart.com>
2017-02-09 23:50:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function () { alert('Hey!') } )
+};
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    key = parseInt(e.which || e.detail)
+    if(key === 71) {
+      alert('g is pressed')
+    }
+  });
+};
+
+function frameIt() {
+  $('img').addClass('tasty')
+};
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  });
+};
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  pressIt();
+  frameIt();
+  submitIt();
 });
