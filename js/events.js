f418802150376c8135bd024418a0f48d7bc3be59
Sammy Steiner <sammysteiner@gmail.com>
2017-02-14 04:33:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img:last').on('load', function() {
+    $('img:last').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('form input:first-child').on('keydown', function(key) {
+    var key = parseInt(key.which)
+    if(key === 71) {
+      alert("You pressed it!")
+    }
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
 
 });
