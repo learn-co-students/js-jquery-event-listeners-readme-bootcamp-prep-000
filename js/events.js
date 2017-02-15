3dd6cdfd840e558205de84220c3c7dabe40816b5
JayKamana <jaykamana@gmail.com>
2017-02-15 07:22:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
+var getIt = function(){
+    $("p").on('click', function(){
+      alert("Hey");
+    })
+}
+
+var frameIt = function(){
+    $("img").on('load', function(){
+        $(this).addClass("tasty");
+    })
+}
+
+var pressIt = function(){
+    $("input[type='text']").on('keydown', function(key){
+      if(key.which === 71)
+        alert("Hey");
+    })
+}
+
+var submitIt = function(){
+    $("form").on('submit', function(){
+      alert("Your form is going to be submitted now.");
+    })
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submit();
 
 });
