03046d2f461e17caa69364e1b1ea66cb36beec9f
Jason  Rosso <jasonrosso@gmail.com>
2017-02-19 03:16:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,38 @@
 $(document).ready(function(){
 
 // call functions here
+$('h1').on("click", function(){
+  //action you want to happen
+});
+
+$('img').on('load',function(){
+//action you want to happen
+
+});
+//keypress
+$(document).on('keypress',
+function(key){
+  if (key.which == 13){
+    alert('enter was pressed');
+  }
+});
+
+//keyup
+$(document).on('keyup',
+function(key){
+  if (key.which==82){
+    alert('r was pressed');
+  }
+
+});
+//keydown$
+$(document).on('keydown',
+function(key){
+  if(key.which==83){
+    alert('s was pressed');
+}
+
+  });
+
 
 });
