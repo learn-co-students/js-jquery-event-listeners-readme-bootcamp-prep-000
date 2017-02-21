684f266edb93e471737ea2c1ada549992b361811
Noah Guillory <slokomisu4@gmail.com>
2017-02-21 05:55:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').load(() => {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', (key) => {
+    if (key.which == 71) {
+      alert('g was pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 // call functions here
 
 });
