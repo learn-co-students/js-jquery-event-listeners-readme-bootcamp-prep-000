4f638f20c0178b5976eb236c468fbc825725f1ac
Andrew Glassett <andrewglassett@gmail.com>
2017-02-22 13:21:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
 
 });
+
+
+
+
+// $(document).ready(function(){
+//   $("p").on("click", function getIt() {
+//     alert('Hey!')
+//   });
+// });
+// $("form").on("submit", function() {
+//   if ($( "input:first" ).val() === "correct") {
+//     alert('your form is going to be submitted now');
+//     return;
+//   }
+//   alert("you entered the wrong value");
+//   return;
+// });
