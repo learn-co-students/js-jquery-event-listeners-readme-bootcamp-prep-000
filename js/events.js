9f098111813d3e3dd3329eaee23c3cbf839b931b
Rafael Perales <ralph.ap333@gmail.com>
2017-02-25 01:29:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('this').addClass('tasty')
+  });
+  return;
+}
+
+function pressIt() {
+  $('document').on('keydown', function() {
+    if(e.which === 71) {
+      alert('g key was pressed.')
+    })
+  });
+  return;
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
