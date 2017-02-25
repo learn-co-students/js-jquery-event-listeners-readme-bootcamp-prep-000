2983d08e7d863a0d2c29ae561248ab2f9b5f9991
Rafael Perales <ralph.ap333@gmail.com>
2017-02-25 01:36:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,13 +1,4 @@
 //define functions here
-
-$(document).ready(function(){
-
-// call functions here
-getIt();
-frameIt();
-pressIt();
-submitIt();
-});
 function getIt() {
   $('p').on('click', function() {
     alert('Hey!')
@@ -35,3 +26,9 @@ function submitIt() {
     alert('Your form is going to be submitted now.')
   })
 }
+
+$(document).ready(function(){
+
+// call functions here
+getIt;
+});
