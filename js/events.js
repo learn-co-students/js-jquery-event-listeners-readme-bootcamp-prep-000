77f5066d3f4684b779b8f13d1071c28ba686709c
stephen chun <stephenchun91@gmail.com>
2017-02-28 19:54:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,38 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(e) {
+      alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(e) {
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(e) {
+    if (e.which === 71 || e.detail === 71) {
+      alert('you pressed g!');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+
+
+
+
+
 $(document).ready(function(){
+
 
 // call functions here
 
