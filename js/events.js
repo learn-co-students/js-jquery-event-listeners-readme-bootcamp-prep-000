236d3e3d8b2a4db2c4ba170ec7103bf2ea04353b
Peter Helstrom <phelstrom@gmail.com>
2017-02-28 12:47:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
-//define functions here
+function getIt() {
+   $('p').on('click', () => alert("hey!"));
+}
+
+function frameIt() {
+     $(document).on('load', () => $('img').addClass("tasty"));
+}
+
+function pressIt() {
+  $('#typing').on('keydown', e => {
+    if((e.detail || e.which)==71)
+      alert("You pressed 'g'!");
+  });
+}
+
+function submitIt() {
+   $('form').on('submit', () => alert("Your form is going to be submitted now."));
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
