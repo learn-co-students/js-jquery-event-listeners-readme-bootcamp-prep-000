c8aec62ea9e063380d70624fe0a6e127ac9cb366
Alexander Hodes <alexander.hodes@live.com>
2017-03-01 18:56:06 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt () {
+  $('p').on("click", function () {
+    alert("Hey!");
+  });
+}
+
+function frameIt () {
+  $(document).on("load", function () {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt () {
+  $('#typing').on("keydown", function (key) {
+    if (key.which == 71) {
+      alert("g was pressed");
+    }
+  });
+}
+
+function submitIt () {
+  $('form').on("submit", function () {
+    alert("Your form is being submitted now.");
+  }
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressit();
+  submitIt();
 });
