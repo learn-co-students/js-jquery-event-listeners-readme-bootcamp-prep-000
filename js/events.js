9a3148dab6c9a5cdb59d997665f6da8d8ba7b8f3
Bryon Martinez <martinez.bryon@gmail.com>
2017-03-01 01:53:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+  return alert("Hey!")
+}
+
+function frameIt() {
+  addClass("tasty")
+}
+
+function submitIt() {
+  return alert("Your form is going to be submitted now.")
+}
+
+/*function pressIt() {
+  const key = $('input').keydown()
+  if (key === 73) {
+      return alert("You have pressed 'g'!")
+    }
+}*/
 
 $(document).ready(function(){
 
 // call functions here
+$('p').click(getIt)
+
+$('img').load(frameIt)
+
+$('form').submit(submitIt)
+
+//$('input').keydown(pressIt)
 
 });
