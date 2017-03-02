0a466bbf8e9f11e97f1f4efb06873cd4ffdb90d6
Bryon Martinez <martinez.bryon@gmail.com>
2017-03-02 01:39:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,32 +1,38 @@
 //define functions here
 function getIt() {
-  return alert("Hey!")
+  $('p').click(function() {
+    return alert("Hey!")
+  })
 }
 
 function frameIt() {
-  addClass("tasty")
+  $('img').load(function() {
+    $('img').addClass("tasty");
+  })
 }
 
 function submitIt() {
-  return alert("Your form is going to be submitted now.")
+  $('form').submit(function() {
+    return alert("Your form is going to be submitted now.")
+  })
 }
 
-/*function pressIt() {
-  const key = $('input').keydown()
-  if (key === 73) {
-      return alert("You have pressed 'g'!")
+function pressIt() {
+  $('input').keydown(function(key) {
+    const keyInt = parseInt(key.which || key.detail)
+    
+    if (keyInt === 71) {
+      return alert("Wow!  You pressed the 'g' Key!")
     }
-}*/
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
-$('p').click(getIt)
-
-$('img').load(frameIt)
-
-$('form').submit(submitIt)
-
-//$('input').keydown(pressIt)
+pressIt();
+frameIt()
+submitIt()
+pressIt()
 
 });
