19982af59528ffdf66ca9f18ad5d236c30e39e16
Dennis ouyang <den2net@yahoo.com>
2017-03-06 03:29:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+  })
+}
 
-$(document).ready(function(){
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+    if(e.which == 71) alert("You have pressed down on the g key!");
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
 
 // call functions here
-
-});
+getIt();
+frameIt();
+pressIt();
