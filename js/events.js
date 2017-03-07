b4aa382d7464cf6be9782b2e818fc4d4cd029f2e
Timothy Ege <retibra@gmail.com>
2017-03-07 20:29:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert("hey!");
+  })
+}//define functions here
 
+function frameIt() {
+  $('img').on("load",function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+  if (key.which === 71){
+    alert("You have pressed the 'g' key.");
+  }
+})
+}
 $(document).ready(function(){
-
+getIt();
+submitIt();
+pressIt();
+frameIt();
 // call functions here
 
 });
