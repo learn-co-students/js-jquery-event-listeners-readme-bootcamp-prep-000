0b5821af195202b8eab90c27a140e86a54da4b0d
Thuy Dao <thuythdao@gmail.com>
2017-03-14 15:38:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+};
+
+function frameIt() {
+  $('img').on('load', function(){
+    $(this).addClass("tasty")
+    $(this).css('border', "solid 5px red")
+  });
+};
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert("You have pressed the 'g' key.")
+    }
+  });
+};
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+};
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
