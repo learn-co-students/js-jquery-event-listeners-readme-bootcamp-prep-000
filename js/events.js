bc471a0a66a7c812fd4981a5b988447adaf2e56d
Trey Boyer <wwboyer14@mail.strakejesuit.org>
2017-03-17 02:52:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => alert("Hey!"))
+}
+
+function frameIt() {
+  $('img').on('load', () => $('img').addClass('tasty'))
+}
+
+function pressIt() {
+  $('input').on('keydown', (e) => {
+    if(e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
