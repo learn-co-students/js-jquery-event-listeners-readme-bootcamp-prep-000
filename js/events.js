fd66015cb1c6cba0f5e15ad43ef2066d6d65893d
Tony Distinti <adistinti@gmail.com>
2017-03-17 15:29:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,46 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+    //$('img').addClass('.tasty')
+    //$('img').append('tasty')
+    //$('img').append('.tasty')
+    //addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key){
+    if (key.which === 71) {
+      alert('g was pressed!')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    //if ($( "input:first" ).val() === "correct") {
+      alert('Your form is going to be submitted now.')
+    //}
+  })
+}
+//Below is what they mean by defining your functions outside document ready.  Then you trigger the functions below this line.
 $(document).ready(function(){
 
 // call functions here
+getIt()
+
+frameIt()
+
+pressIt()
+
+submitIt()
 
 });
