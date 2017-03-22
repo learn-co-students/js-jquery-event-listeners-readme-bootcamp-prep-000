9b1e751477e3c22d903f1d1d4620292557a63f11
Brian Chou <bchou515@gmail.com>
2017-03-22 00:18:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
 
-// call functions here
+function pressIt() {
+  $(document).on('keydown', function (event) {
+    const key = Number.parseInt(event.which || event.detail)
+    if (key === 71) {
+      alert('G HAS BEEN PRESSED')
+    }
+  })
+}
 
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+$(document).ready(function() {
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
