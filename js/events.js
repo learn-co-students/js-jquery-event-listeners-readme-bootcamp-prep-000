39100397aa1ecfd4fe8f51443bd9d58b5f4940c1
Marcelo Coirazza <marcelocoirazza@gmail.com>
2017-03-25 02:10:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+
+function getIt() {
+	$("p").on("click", function (e) {
+		alert('Hey!')	})
+}
+
+function frameIt() {
+	$("img").on('load', function () {
+		$("img").addClass('tasty');
+		$("img").css('border', '1px, solid, #BA5757');
+	})
+}
+
+function pressIt() {
+	$("#typing").on('keydown',function(e) {
+		if (e.which === 71){
+			alert('You have pressed the `g` key.')
+		}
+	})
+
+}
+
+function submitIt() {
+	$("form").on('submit',function(){
+		alert('Your form is going to be submitted now.');
+	});
+}
