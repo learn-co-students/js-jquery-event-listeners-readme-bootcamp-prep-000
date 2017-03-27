ed391caeb6ee511933b440178efd730ce8348010
Tyler Gotto <tylergotto17@gmail.com>
2017-03-27 09:51:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+    $('p').on('click', function(){
+        alert("Hey!");
+    })
+}
+function frameIt() {
+    $('img').on('load', function(){
+        $('img').addClass('tasty');
+    })
+}
+function submitIt() {
+      $('form').on('submit', function() {
+      alert("Your form is going to be submitted now.");
+  })
+}
+function pressIt() {
+      $('input').on('keydown', function(e) {
+        if (parseInt(e.detail || e.which) === 71 ) {
+            alert("You pressed the G key!");
+        }
+      })
+}
 
 $(document).ready(function(){
-
 // call functions here
+    getIt();
+    frameIt();
+    submitIt();
+    pressIt();
 
 });
