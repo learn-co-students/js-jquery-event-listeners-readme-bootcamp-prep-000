e968e102000939e64b3cf3afb1f288799625762e
Killian Piraro <kpiraro@gmail.com>
2017-03-28 14:26:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $(this).addClass('tasty');
+
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key){
+    if (key.which == 71) {
+      alert('G!')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
+getIt()
+frameIt()
+pressIt()
+submitIt()
 // call functions here
 
 });
