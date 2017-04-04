1f80e3b23bd29b36178cd4a1eda0b14f65ee87fb
David Schlauderaff <dschlaud@gmail.com>
2017-04-04 00:46:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(key){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(key){
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if (key.which == 71) {
+      alert('g was pressed')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on("submit", function(key){
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
