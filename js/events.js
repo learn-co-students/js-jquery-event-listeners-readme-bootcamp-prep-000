3012cbf060a6be878601259134ab0da1a991e395
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2017-04-10 02:16:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
+function getIt() {
+  $('p').click(function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71 || e.which === 103) { // G or g
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 $(document).ready(function(){
 
-// call functions here
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
