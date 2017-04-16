13f8e52258bbd82c0fc3efc1916d8e63ce6107ec
Saif Mahmud <smahmud46@yahoo.com>
2017-04-16 06:04:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,41 @@
 
 $(document).ready(function(){
 
+    getIt();
+
+   frameIt();
+
+   pressIt();
+
+   submitIt();
+
 // call functions here
 
 });
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input#typing').keydown(function(key) {
+    if (key.which === 71) {
+      alert("You have pressed the G key.")
+    }
+  });
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert("Your form is going to be submitted now.");
+  })
+
+}
