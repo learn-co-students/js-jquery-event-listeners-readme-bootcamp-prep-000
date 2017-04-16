c24c6f5323395a42b2b4de64485ca0cbea802bc2
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-16 18:52:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,10 @@
 //define functions here
+//learn open js-jquery-event-listeners-readme-bootcamp-prep-000
 
+var getIt = () => {
+  $('p').appand("click");
+  alert("Hey!");
+}
 $(document).ready(function(){
 
 // call functions here
