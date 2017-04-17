c7ab6bc5c4ce40b7643c59b87560eb5261b9fc6f
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-17 22:07:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -8,11 +8,30 @@ var getIt = () => {
 
 var frameIt = () => {
   $('img').on('load' , function() {
-    $('img').prepend(.tasty);
+    $("img").addClass("tasty");
   });
 }
-$(document).ready(function(){
 
+var pressIt = () => {
+    $('form input:first-child').on('keydown' , function(e){
+        var key = parseInt(e.detail || e.which);
+        if(key === 71)
+            alert('G was prest');
+    });
+}
+
+var submitIt = () => {
+  $("form").on("submit", function() {
+      alert('Your form is going to be submitted now.');
+      return;
+    });
+}
+
+  $(document).ready(function(){
+
+    frameIt();
+    submitIt();
+    //pressIt();
 // call functions here
 
 });
