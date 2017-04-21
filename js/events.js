3776388ef732ac591e3fcd83891b9ea4f4fa248f
Suhan Wijaya <suhanw@gmail.com>
2017-04-21 14:50:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,32 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+
+function getIt(){
+  $("p").on("click", function() {
+    alert ("Hey!");
+  })
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("input#typing").on("keydown", function(key){
+    if(key.which === 71){
+      alert("You pressed G!");
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", alert("Your form is going to be submitted now."));
+}
