4b49c4230c185b1cc196bac86b73821ee770d110
Anish Kumar Shaw <anishshaw100@gmail.com>
2017-04-21 14:11:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function (key){
+    alert('"Hey!"');
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(key){
+    
+  })
+}
+
+function pressIt(){
+  $("form").on('keydown', function(key) {
+    if (key.which == 71){
+      alert('You have entered g');
+    }
+    })
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
