5cafdd39fd133ec6f836be33a3c73eaf4c88802d
Natalie Dean <ndean87@gmail.com>
2017-05-06 21:13:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,27 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+        alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').addClass('tasty').on('load', function() {
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which === 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
