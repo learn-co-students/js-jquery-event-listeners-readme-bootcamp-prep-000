492a5891d3b6cfb8c341869ef9f757548a93c95a
Brian Lee <blees.email@gmail.com>
2017-05-07 01:43:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+  alert("Hey!");
+})}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+function pressIt() {
+  $("input").on("keydown", function(key) {
+    if(key.which == 71) {
+      alert("Alerty alert");
+      return;
+    }
+    return;
+  })
+}
 
 $(document).ready(function(){
-
+getIt();
+frameIt();
+submitIt();
+pressIt();
 // call functions here
 
 });
