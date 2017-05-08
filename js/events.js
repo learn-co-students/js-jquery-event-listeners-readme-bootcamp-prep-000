efa8338ed84514d0c7ddfb61e59d39edd4df5d6e
Jonathan Penton <jonathan@penton.site>
2017-05-08 04:36:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
+function frameIt() {
+  $('img').on('load', function(e) {
+    $('img').addClass('tasty')
+  })
+}
+
+function getIt() {
+  $('p').on('click', function(e) {
+    alert('Hey!')
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(e) {
+    let key = parseInt(e.which || e.detail)
+    if (key === 71) {
+      alert('You have pressed \'G\'.')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
-// call functions here
+  // call functions here
 
 });
