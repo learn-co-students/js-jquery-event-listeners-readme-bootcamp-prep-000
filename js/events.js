6e53c279260e0b54b3c757e54d5b132b7e5f3420
JD Leshan <stritch@gmail.com>
2017-05-09 19:46:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,33 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")});
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key){
+        if(key.which == 71){
+          alert('g was pressed');
+        }
+  })
+}
+
+function submitIt (){
+    $('form').on('submit',function(){
+       alert("Your form is going to be submitted now.")
+      }
+    )
+}
+
+function frameIt(){
+    $('img').addClass("tasty");
+}
