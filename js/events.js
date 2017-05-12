0e5e0b06c920474e7273a8f5af2832821ad6d8e1
Pradeep Dayaram <vickdayaram@gmail.com>
2017-05-12 16:02:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+submitIt()
+pressIt()
 
 });
+
+function getIt(){
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function pressIt(){
+  $('form input:first-child').on('keydown', function(key){
+    if(key.which == 71){
+      alert('the g key')
+    }
+  })
+}
