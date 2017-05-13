4f37353b0db50baaf2093767459232c99cd2d899
Brian Hays <brian.hays@gmail.com>
2017-05-13 23:06:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  return $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
 
-$(document).ready(function(){
+function frameIt() {
+  return $('img').on("load", function() {
+    $('img').addClass("tasty");
+  });
+}
 
-// call functions here
+function submitIt() {
+  return $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
+function pressIt() {
+  return $('form').on("keydown", function(key) {
+    if(key.which == 71) {
+      alert('g was pressed');
+    }
+  });
+}
+
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
