b6ffd24e3043d033a492c6ebd1308706fd021f8d
Ginger Wilson <connor.wilson221@gmail.com>
2017-05-14 00:40:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
+function getIt(){
+  $('p').on("click", function(){
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function(){
+    $("img").addClass("tasty")
+ });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('Your form is going to be submitted now.');
+ });
+}
+
+function pressIt(){
+  $("input").on('keydown', function(e){
+    if(e.which===71){
+      alert()
+    }
+  })
+}
 //define functions here
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt()
+  frameIt()
+  submitIt()
+  pressIt()
 });
