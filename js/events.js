658fa2b5c75ecb2a58d5517edeaac16eaa99a802
Shaun McLoughlin <skmcloughlin@gmail.com>
2017-05-16 16:39:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
 //define functions here
+function getIt() {
+  alert('Hey!');
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').addClass('tasty');
+}
 
-// call functions here
+function pressIt() {
+  alert('You have pressed the G key.');
+}
 
+function submitIt() {
+  alert('Your form is going to be submitted now.');
+}
+
+$(document).ready() {
+  $('p').on('click', getIt());
+  $(document).on('load', frameIt());
+  $('#input').on('keydown' === 71, pressIt());
+  $('form').on('submit', submitIt());
 });
