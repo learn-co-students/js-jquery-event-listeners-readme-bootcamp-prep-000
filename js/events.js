8343ee776ebd686459c4cc8fd448a4b83417dcc3
Jared Mitchell <jared.mitchell@yahoo.com>
2017-05-17 22:59:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
-//define functions here
+function getIt() {
+  $('p').on('click',function() {
+    alert('Hey!');
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load',function() {
+    $('img').attr('class','tasty');
+    return;
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown',function(key) {
+    if(key.which === 71) {
+      alert("'g' key pressed");
+    }
+    return;
+  })
+}
+
+function submitIt() {
+  const form = window.$('form')[0]
+  window.$(form).on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
 
 $(document).ready(function(){
-
 // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
