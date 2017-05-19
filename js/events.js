d3320be1a0e1774df74a7dd1583cf2a9adba697e
Robert Gonzalez <robertgonzalez@utexas.edu>
2017-05-19 01:29:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function() {
+alert("Hey!")
+    })
+  return
+}
+
+function frameIt(){
+$('img').on('load', function(){
+$('img').addClass('tasty')
+  })
+  return
+}
+
+function pressIt(){
+$('input').on('keydown', function(key){
+ if(key.which === 71){
+  alert('you pressed g')
+  }
+   })
+  return
+}
+
+function submitIt(){
+$('form').on('submit', function(){
+alert('Your form is going to be submitted now.')
+  })
+  return
+}
 
 $(document).ready(function(){
-
-// call functions here
-
 });
