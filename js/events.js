f53789b44dd56f00f07221e29ffede4358959809
LS C <suzy.cho111@gmail.com>
2017-05-20 06:31:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,53 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which===71) {
+      alert("You entered letter G");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+/*$('p').on('click', getIt);
+$('img').on('load', frameIt);
+$('input').on('keydown', pressIt);
+$('form').on('submit', submitIt);
+
+function getIt() {
+    alert("Hey!");
+}
+
+function frameIt() {
+    $('img').append('<img class='tasty'/>');
+}
+
+function pressIt(key) {
+    if(key.which===71) {
+      alert("You entered letter G.");
+    }
+}
+
+
+function submitIt() {
+    alert("Your form is going to be submitted now.");
+}
+
+*/
 $(document).ready(function(){
-
-// call functions here
 
 });
