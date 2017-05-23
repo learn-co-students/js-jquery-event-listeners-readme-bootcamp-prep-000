e3c96aa8107f2950094557139bb448bf68a12741
Narcita Hamilton <majesticwaterfall11@yahoo.com>
2017-05-23 06:24:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,43 @@
-//define functions here
+function getIt(){
+ $(document).ready(function(){
+   $('p').on('click', function getIt(){
+     alert("Hey!");
+   }
+   );
+ }
+ );
+  }
 
-$(document).ready(function(){
 
-// call functions here
+function frameIt(){
+  $(document).ready(function(){
+    $('img').on('load', function frameIt() {
+     $('img').addClass('tasty');
+    }
+    );
+   }
+   );
+}
 
-});
+function pressIt(){
+  $(document).ready(function(){
+    $('form').on('keydown', function pressIt(key) {
+     if(key.which==71)
+       alert('g was pressed')
+     }
+     );
+    }
+    );
+   }
+
+
+ function submitIt() {
+  $(document).ready(function() {
+    $('form' ).on('submit', function(submitIt) {
+      alert("Your form is going to be submitted now.");
+
+     }
+    );
+    }
+  );
+}
