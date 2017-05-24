1fb31cbe6452c221b6422f6d5074a884a831facb
Katerina  Papacostas <katpapacostas@gmail.com>
2017-05-24 15:54:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
 
-$(document).ready(function(){
 
-// call functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
 
-});
+function frameIt() {
+  $('img').on('load', function(e) {
+    $(e.target).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if(parseInt(e.which) === 71) {
+      alert('you pressed G')
+    }
+  })
+}
+
+function submitIt() {
+    $("form").on('submit', function() {
+      alert('Your form is going to be submitted now.')
+    })
+}
+
+$('document').ready(function(){
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+})
