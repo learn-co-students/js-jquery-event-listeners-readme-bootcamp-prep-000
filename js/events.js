f6e07712c9d67f6ecc35016fa128ae0b51b2a752
william hancharek <william.hancharek@gmail.com>
2017-05-26 03:09:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -9,7 +9,6 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keydown', function(e) {
-    // 'g' corresponds to 71
     if (e.which === 71) {
       alert("You pressed 'g'!")
     }
