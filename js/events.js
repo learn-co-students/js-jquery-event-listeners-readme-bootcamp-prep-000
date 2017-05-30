1e9dc9497917e804df7d3d46894cf82525528684
Matthew Croak <mattcroak718@gmail.com>
2017-05-30 00:57:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt(){
+  $('p').on("click", function(){
+      alert('hey!');
+      return;
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass ('tasty');
+    return;
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if (key.which == 71){
+      alert('g was pressed');
+      return;
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+}
 
 $(document).ready(function(){
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 // call functions here
 
 });
