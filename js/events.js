3f3354333fc18a069e8e6fd27880f02e2ea4af9a
Nolan Kitchin <nolankitch@gmail.com>
2017-05-30 23:53:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,48 +1,45 @@
 //define functions here
 
 function getIt() {
-  alert("Hey!")
+  $('p').on("click", function(){
+      alert("Hey!")
+  });
 }
 
 function frameIt() {
-  $('img').addClass('tasty')
+  $(document).on('load', function(){
+      $('img').addClass('tasty')
+  });
 }
 
 
 function pressIt() {
-    /*if ($( "input:first" ).val() === "g") {
-      alert('g was pressed');
-      return;
-    } */
-    alert('g was pressed');
-    return;
+  $('#typing').on('keydown', function(key) {
+    if(key.which === 71){
+        alert('g was pressed');
+        return;
+      } else {
+        return;
+      }
+  });
+
 }
 
 
 function submitIt() {
-  alert('Your form is going to be submitted now.')
+  $("form").on("submit", function() {
+      alert('Your form is going to be submitted now.')
+  });
 }
+
+
 
 $(document).ready(function(){
 
 // call functions here
-$('p').on("click", function(){
-    getIt();
-});
-
-$(document).on('load', function(){
-    frameIt();
-});
-
-$('#typing').on('keypress', function(key) {
-  if(key.which === 71){
-      pressIt();
-      //alert('g was pressed')
-    }
-});
-
-$("form").on("submit", function() {
-  submitIt();
-});
+getIt();
+frameIt();
+pressIt();
+sendIt();
 
 });
