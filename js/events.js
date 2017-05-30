0c726a1a6db4b4f6fb971dbed72a2beb7e0bd38c
Nolan Kitchin <nolankitch@gmail.com>
2017-05-30 05:46:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,48 @@
 //define functions here
 
+function getIt() {
+  alert("Hey!")
+}
+
+function frameIt() {
+  $('img').addClass('tasty')
+}
+
+
+function pressIt() {
+    /*if ($( "input:first" ).val() === "g") {
+      alert('g was pressed');
+      return;
+    } */
+    alert('g was pressed');
+    return;
+}
+
+
+function submitIt() {
+  alert('Your form is going to be submitted now.')
+}
+
 $(document).ready(function(){
 
 // call functions here
+$('p').on("click", function(){
+    getIt();
+});
+
+$(document).on('load', function(){
+    frameIt();
+});
+
+$('#typing').on('keypress', function(key) {
+  if(key.which === 71){
+      pressIt();
+      //alert('g was pressed')
+    }
+});
+
+$("form").on("submit", function() {
+  submitIt();
+});
 
 });
