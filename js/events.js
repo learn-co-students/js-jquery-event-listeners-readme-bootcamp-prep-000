a55cc3f24776a60eebea3a340e1808b946a82af2
Greg Dwyer <gjdwyer8@gmail.com>
2017-05-30 21:57:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,27 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load').addClass('tasty');
+  }
+
+  function pressIt(){
+    $('form').on('keydown', function(key){
+      if(key.which == 71) {
+        alert("g key was pressed down");
+      }
+    });
+  }
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
