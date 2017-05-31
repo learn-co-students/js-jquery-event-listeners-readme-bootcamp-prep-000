0ab01756ecf6979752131a1a6f7becb70a4ff66f
Ben Folk-Sullivan <benfolksullivan@gmail.com>
2017-05-31 17:30:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt(){
+  alert("Hey!")
+}
+
+function frameIt(){
+  $('img').addClass("tasty")
+}
+
+function submitIt(){
+  alert('Your form is going to be submitted now.')
+}
+
+function pressIt(){
+  $(document).on("keydown", function(key){
+  if (key.which === 71){
+    alert('g was pressed')
+  }
+})
+}
 
 $(document).ready(function(){
 
 // call functions here
+$('p').on('click', getIt)
 
-});
+$('img').on('load', frameIt)
+
+$('form').on("submit", submitIt)
+
+pressIt()
+
+
+
+} );
