a2bac88f38758f1a3a1c62d67835d6ccc02b3c4f
Anderson Manosalva <tripkler@hotmail.com>
2017-06-02 18:08:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt() {
+  $('p').click(function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').load(function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').keydown(function(key){
+    if (key.which == '71') {
+      alert('"g" was pressed.')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+//____________________________
 
 $(document).ready(function(){
-
 // call functions here
-
+return getIt();
+return frameIt();
+return pressIt();
+return submitIt();
 });
