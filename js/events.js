543445a79cef0d92b9aa83d1712619d6f85128d3
Paul Hosler <pahosler@gmail.com>
2017-06-03 18:23:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
-//define functions here
-
 $(document).ready(function(){
 
-// call functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').ready(function(){
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $('input:first').on("keyup",function(key){
+    if(key.which === "G".charCodeAt()) {
+      alert("G key pressed!");
+      $('input:first').val('correct')
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on("submit",function(){
+    if ($('input:first').val() === "correct") {
+      alert("Your form is going to be submitted now.");
+      return;
+    }
+    alert("You entered the wrong value.");
+    return;
+  });
+}
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
