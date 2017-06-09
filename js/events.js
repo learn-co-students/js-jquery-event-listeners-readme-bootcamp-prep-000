507a8c8230687de1a8e518fdadbaeeab2a196d5c
Elaine Lai <elainejlai@gmail.com>
2017-06-09 00:21:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(key) {
+    alert('Hey!')
+    })
+}
+
+function frameIt(){
+  $('img:first').on('load', function(){
+    $('img:first').addClass("tasty")
+  })
+}
+
+function submitIt(){
+  $("form").on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $("form").on('keydown', function(key){
+    if(key.which == )
+
+  })
+}
+
+$(document).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+});
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+submitIt()
 });
