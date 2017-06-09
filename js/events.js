8260d8e749f9a7ff7a74c508893425a3229f986b
Andrew James <ajames20@gmail.com>
2017-06-09 15:15:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
 
-$(document).ready(function(){
-
-// call functions here
-
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
+
+function getIt(params) {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt(params) {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+    }
+  });
+}
