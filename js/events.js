c528ff668e925b67f5282c873156a48a798c1010
chris u <cu364@nyu.edu>
2017-06-22 23:52:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
 
 $(document).ready(function(){
 
-// call functions here
-
 });
+
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+    $('img').css ('border','1px solid red')
+  });
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(key) {
+    if (key.which == 71) {
+      alert("What a G");
+    }
+  });
+}
+
+
+function submitIt() {
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+    return
+  })
+}
