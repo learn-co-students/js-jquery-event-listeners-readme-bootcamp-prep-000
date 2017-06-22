083dfe7f533c1f4164d69b173e4b3fe11009e794
david  he <ximin.he184@gmail.com>
2017-06-22 20:54:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
-//define functions here
+function getIt(){
+  $('p').on('click', alert('Hey!'))
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if (key.which === 71){ alert('Your form is going to be submitted now.')};})
+}
+function submitIt(){
+  $('input').on('submit', alert('Your form is going to be submitted now.'))
+
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
 
+frameIt()
+
+pressIt()
+
+submitIt()
 });
