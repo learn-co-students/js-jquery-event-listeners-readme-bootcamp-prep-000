e8347ba70c706e1be8f780517eeeda21afc28177
Joanna Sese <jmgsese@gmail.com>
2017-06-26 21:52:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
 $(document).ready(function(){
-
 // call functions here
 
 });
+
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which === 71){
+      alert('G was pressed.')
+    }
+  })
+}
+
+function submitIt(){
+  $(document).on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
