4070667f2e3c9f6098b404340f62a5f6207c47af
Melissa Amaya <amaya.melissa@gmail.com>
2017-06-29 20:33:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,22 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
 
+function frameIt(){
+  $('img').on('load', function(){
+    $("img").addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if (key === 103){
+      alert('You have pressed "g"')
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
