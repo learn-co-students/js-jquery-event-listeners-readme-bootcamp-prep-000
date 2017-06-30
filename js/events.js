da3a361ad4d0fb278becebf59995cdf0773c9c07
Bobby Huang Huang <zhenghuahuang@gmail.com>
2017-06-30 02:47:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,31 @@
 
 $(document).ready(function(){
 
-// call functions here
+  $("p").on("click", getIt);
+  $("document").on("load", frameIt);
+  $("#typing").on("keydown", function(e) {
+    pressIt(e.which);
+  });
+  $("form").on("submit", submitIt);
 
 });
+
+function getIt() {
+  alert("Hey!");
+}
+
+function frameIt() {
+  $("img").addClass("tasty");
+}
+
+function pressIt() {
+  $("#typing").on("keydown", function(e) {
+    if (e.which == 71) {
+      alert("G Pressed!");
+    }
+  });
+}
+
+function submitIt() {;
+  alert("Your form is going to be submitted now.");
+}
