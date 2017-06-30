acf3c6b3d2d240438db994d1b47167e158603908
Oliver Curting <oc@hejverden.dk>
2017-06-30 02:26:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
 
+// 1.
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+// 2.
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+// 3.
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert("You pressed the G-button")
+    }
+  })
+}
+
+// 4.
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
