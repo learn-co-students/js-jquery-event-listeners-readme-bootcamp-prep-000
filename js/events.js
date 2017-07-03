ffe9ad922f70de630b4da323f07ce26c77a4ecc1
Kevin Huang <yellow.khaos@gmail.com>
2017-07-03 02:47:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,36 +1,36 @@
 //define functions here
 
-function getIt(){
-  $('p').do('click',function(){
+function getIt() {
+  $('p').on('click', function() {
     alert("Hey!");
   });
   return;
 }
 
-function frameIt(){
-  $('img').do('load', function(){
-    #('img').addClass('tasty');
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
   });
   return;
 }
 
-function pressIt(){
-  #('form').do('keydown', function(){
-    if(key.which==='71'){
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which === 71) {
       alert("what!?!");
     }
   });
   return;
 }
 
-function submitIt(){
-  #('form').do('submit', function(){
+function submitIt() {
+  $('form').on('submit', function() {
     alert("Your form is going to be submitted now.");
   });
   return;
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   getIt()
   frameIt()
   pressIt()
