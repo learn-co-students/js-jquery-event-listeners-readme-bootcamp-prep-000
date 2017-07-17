82556d062ff9b08b56cc0344ca65de8b4c15642a
Gene Yoo <gene.y.yoo@gmail.com>
2017-07-17 03:19:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which === 71) {
+      return alert("You typed the letter G!")
+    }
+    return
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
