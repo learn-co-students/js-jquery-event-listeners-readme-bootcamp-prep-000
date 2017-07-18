e10f177938e24ad884ec2cb25b9ce46600630c0f
kylek <kbelway29@gmail.com>
2017-07-18 18:17:30 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,20 @@
 //define functions here
+function getIt() {
+  alert("Hey!")
+}
+function frameIt() {
+  $('img').on("load", function(img.className += " tasty"))
+}
+
+function pressIt() {
+
+}
+
+function submitIt() {
+
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  $('p').on("click", function getIt() {}
 });
