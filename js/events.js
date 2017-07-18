eb32ab43cea5a9ae62d220c9983bd989e29cdabf
kylek <kbelway29@gmail.com>
2017-07-18 19:10:14 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -5,23 +5,29 @@ function getIt() {
   })
 }
 function frameIt() {
-  $('img').on("load", function(img.className += " tasty"))
+  $('img').on("load", function() {
+    $('img').addClass("tasty")
+  })
 }
 
 function pressIt() {
-
+  $('#typing').on("keydown", function(key) {
+    if(key.which == 80){
+      alert('g was pressed')
+    }
+  })
 }
 
 function submitIt() {
-
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
 }
 
 $(document).ready(function(){
 // call functions here
-    function getIt() {
-      $('p').on("click", function() {
-        alert("Hey!")
-      })
-    }
-    getIt()
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 };
