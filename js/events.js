f3662cbc551c21c4c154abea839601234127c317
Christina Pan <christina.pl.pan@gmail.com>
2017-07-19 01:06:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,27 +1,30 @@
 //define functions here
+
 function getIt() {
-  $('p').on('click', alert('Hey!'));
-};
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
 
 function frameIt() {
   $('img').on('load', function() {
     $('img').addClass('tasty');
-  });
-};
+  })
+}
 
 function pressIt() {
-    $('#typing').on('keydown', function(key) {
-      if (key.which === 71) {
-        alert("You have pressed G");
-      }
-    });
-};
+  $('form').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert("You pressed the G");
+    }
+  })
+}
 
 function submitIt() {
   $('form').on('submit', function() {
     alert("Your form is going to be submitted now.");
   })
-};
+}
 
 $(document).ready(function(){
 
@@ -30,4 +33,5 @@ getIt()
 frameIt()
 pressIt()
 submitIt()
+
 });
