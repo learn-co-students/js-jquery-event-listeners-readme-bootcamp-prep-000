13856878b11deea5019eda19424ec9e366360f1b
Nick Cianci <n.cianci3@gmail.com>
2017-07-20 01:43:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function (e) {
+    $(e.currentTarget).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function (e) {
+    const gKey = 71;
+    if (parseInt(e.which) === gKey) {
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
