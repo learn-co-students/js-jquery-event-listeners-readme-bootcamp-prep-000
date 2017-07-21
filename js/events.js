1536b69d2924884f3dbb3cf6a75326cb7bf9a285
Marc Smith <marc.allan34@gmail.com>
2017-07-21 17:51:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt(){
+  $("p").click(function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $(document).load(function(){
+    $("img").addClass("tasty")
+  })
+}
+
+function pressIt(){
+  $("#typing").keydown(function(key){
+    if(key.which == 71){
+      alert('You have pressed down the "G" key!')
+    }
+  })
+}
+
+function submitIt(){
+  $("form").submit(function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
