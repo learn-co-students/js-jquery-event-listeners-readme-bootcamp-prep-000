e49c0907a8a03ecce0c0dcea2c51729c89e61420
Archana Burra <burra.archana@gmail.com>
2017-07-25 19:37:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,38 +1,55 @@
 //define functions here
 function getIt(){
-  $('p').on('click', function() {
-    alert("Hey!");
-    return;
-  })
+  $('p').on('click', function() {alert("Hey!")})
+
 }
 
 function frameIt(){
   $('img').on('load', function(){
-    $('img').addClass('tasty')
-    return;
-  }
-)}
-
-function submitIt(){
-  $('form').on('submit', function(){
-    alert("Your form is going to be submitted now.");
-    return;
+    $('img').addClass('tasty');
   })
 }
 
-function pressIt(){
-  $('input').on('keydown', function(key){
-    if(key.which === 71){
-      alert("You pressed the G key");
-      return
-    }
-  }
-
+function submitIt(){
+  $('submit').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  })
 }
+
+function pressIt() {
+  $(document).on('keydown', function(key){
+    if(key.which === 71){
+      alert('g key was pressed down');
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+
+  $('body').on('keypress', function(key){
+    if(key.which === 13){
+    alert('enter was pressed');
+      }
+    });
+
+  $('body').on('keydown', function(key) {
+    if(key.which == 83){
+      alert('s was pressed');
+    }
+  });
+
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('Your form is going to be submitted now.');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+  });
+
 
 
 });
