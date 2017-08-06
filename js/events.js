dc593a75610239a37f018936cc7e019be230dfdf
Adam Gold <golddragonxyz@yahoo.com>
2017-08-06 14:59:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+subitIt();
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').load(function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e){
+    if (parseInt(e.which || e.detail) === 71) {
+      alert("You have pressed the 'G' key!");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
