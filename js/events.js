9d652d2ebb110c502700221a46cdf4f1e493ba2e
beardshadow <beardshadow@gmail.com>
2017-08-07 19:57:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    window.alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if (e.which === 71){
+      window.alert('you pressed the g!');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    window.alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
