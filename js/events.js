a201bc6b9e8a0a3d58ee688bb632f5342abe3f17
Jordan Storms <jcstorms25@gmail.com>
2017-08-07 18:17:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt(){
+  return $('p').on("click", function(){
+    alert("Hey!")
+  })
+}
 
-$(document).ready(function(){
+function frameIt(){
+  return $('img').on("load", function(){
+    $('img').addClass('tasty')
+  })
+}
 
-// call functions here
+function pressIt(){
+  return $('#typing').on("keydown",function(key){
+    if(key.which === 71){
+      alert("You pressed g")
+    }
+  })
+}
 
+function submitIt(){
+  return $('form').on("submit",function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+$('document').ready(function(){
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
