9ee30fd3a9bcf6b08c68bd2ed0c2fc6be0daa8d1
zolkocarbon <eathena@hotmail.com>
2017-08-09 18:28:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {   //when P is clicket call function getIt
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {   //when image loads call function frameIt
+  $('img').on('load', function() {
+    $("img").addClass("tasty");
+});
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+      alert('G was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+   alert('Your form is going to be submitted now.')
+});
+}
+
+
+
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
