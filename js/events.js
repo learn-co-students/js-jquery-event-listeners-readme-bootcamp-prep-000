d19f7f197a60a988565ce661d3ed9b655d0f854c
Aleksandar Šmigić <aleksandar.nexus@gmail.com>
2017-08-11 15:24:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
 //define functions here
+function getIt() {
+  $('p').on('click', e => {window.alert('Hey!')})
+}
+
+function frameIt() {
+  $('img').on('load', e => {$('img').addClass('tasty')})
+}
+
+function pressIt() {
+  $('input[type="text"]').on('keydown', e => {if(e.which == 71) window.alert('You pressed the G')})
+}
+
+function submitIt() {
+  $('form').on('submit', e => {window.alert('Your form is going to be submitted now.')})
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
 });
