d92ea1ac73e4cf46c9ed77c8b8a9717c5063f0e5
LG-STEM <gatesinfo@gmail.com>
2017-08-15 22:06:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').on('click', function(key) {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').attr('class', 'tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
