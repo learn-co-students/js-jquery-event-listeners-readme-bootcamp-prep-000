e9a59eec1f79f2d17c4a3941c7e7840e7d757c91
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-16 16:41:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+  alert("Hey!")//actions you want to happen
+});
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(key){
+    alert("Your form is going to be submitted now.")
+  });
+}
+
+
 
 $(document).ready(function(){
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 // call functions here
 
 });
