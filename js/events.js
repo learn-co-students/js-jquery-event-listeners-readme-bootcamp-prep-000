f473e9155e2ced23ed9a90cdebbb1b5274ec5ccb
Araba Taylor <araba.taylor20@gmail.com>
2017-08-19 23:24:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
-//define functions here
+function getIt(){
+$('p').on('click', function(){
+  alert('Hey!')
+})
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+    if(e.which === 71){
+      alert('You have pressed G!')
+    }
+  })
+}
+
+function submitIt(){
+$('form').on('submit', function(e){
+  alert('Your form in submitted now.')
+})
+}
 
 $(document).ready(function(){
 
-// call functions here
 
-});
+})};
