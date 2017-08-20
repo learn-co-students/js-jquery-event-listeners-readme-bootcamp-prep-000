01c2e4d188ccb92906e6fe165c638201cc0f0017
rjcolmed <rjcolmed@gmail.com>
2017-08-20 17:38:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(event) {
+    if(event.which == 71) {
+      alert('Pressed G...');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
 
 $(document).ready(function(){
 
