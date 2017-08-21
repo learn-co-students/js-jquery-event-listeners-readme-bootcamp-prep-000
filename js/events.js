6a095801f27296981a4bc0abb2e10416cca748c1
Pawel Misiak <pawelmisiak@live.com>
2017-08-21 16:26:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,33 @@
 
 $(document).ready(function(){
 
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+
+function getIt(){
+  $('p').on("click", function(){
+    return alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71)
+    return alert('You have pressed G key')
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    return alert("Your form is going to be submitted now.")
+  });
+}
