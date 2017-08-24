662d08915975d46945304a3b1fff2044c776b303
John Day <itisjohnday@gmail.com>
2017-08-24 17:52:56 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    let img = $('img')
+    img.addClass('tasty')
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  let form = $('form')[0]
+  $(form).on("submit", function() {
+    return alert('Your form is going to be submitted now');
+  });
+}
