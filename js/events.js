06e7c4dc67d1ef3337873592b83ae387313e46d7
dragos busuioc <dmihail1994@gmail.com>
2017-08-24 08:28:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
 
+function getIt() {
+  $('p').click(function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load',function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown',function(press) {
+    if (press.which === 71) {
+      alert('You pressed G');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit',function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
+
 $(document).ready(function(){
-
-// call functions here
-
 });
