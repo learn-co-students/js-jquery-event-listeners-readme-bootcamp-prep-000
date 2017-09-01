f154b01a14e1ea370650442f65645f794ff513f4
John O'Connor <oconnojb@gmail.com>
2017-09-01 14:44:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
+function getIt(){
+  alert(`"Hey!"`);
+}
+
+function frameIt(){
+  $('img').addClass('tasty');
+}
+
+function submitIt(){
+  alert(`Your form is going to be submitted now.`);
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+$('p').on('click', getIt());
+$('img').on('load', frameIt());
+$('form').on('submit', submitIt());
+$('#typing').on('keydown', pressIt());
 
 });
