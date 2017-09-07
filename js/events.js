1ef5dc6a9222c56b8d5da5454ba50b235545514f
Kartik Parihar <kartikvips@gmail.com>
2017-09-07 06:40:44 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
 
-$(document).ready(function(){
+$(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
+
+function getIt()
+{
+  $('p').on('click', function(){alert('Hey!')})
+}
+
+function frameIt()
+{
+
+  $('img').on('load', function(){$('img').addClass("tasty")})
+}
+
+function pressIt()
+{
+  $('#typing').on('keydown', function(e){if(parseInt(e.which) === 71){alert('G is pressed!')}})
+}
+
+function submitIt()
+{
+  $('form').on('submit', function(){alert('Your form is going to be submitted now.')})
+}
