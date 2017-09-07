2aee67300c3428d9aa004d4c15fcdc2374a997fa
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-09-07 19:07:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
-//define functions here
+function getIt()
+{
+  $('p').on('click',function(){alert('hey')})
+}
+
+function frameIt()
+{
+$('img').on('load', function(){
+  var a = $('img')
+  a.addClass("tasty")
+})
+}
+
+function pressIt()
+{
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71) {
+        alert("you pressed G")
+      }
+
+  })
+}
+
+function submitIt()
+{
+  $('form').on('submit',function() {alert('Your form is going to be submitted now.')})
+}
+
 
 $(document).ready(function(){
 
