ca13d1cb3ef5ad8c78baa472604a53f607ba70e5
Joshua James <burke.joshua.james@gmail.com>
2017-09-08 03:21:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+    return;
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function (e) {
+    if (e.which === 71) {
+      alert("You pressed G!")
+    }
+    return;
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+}
