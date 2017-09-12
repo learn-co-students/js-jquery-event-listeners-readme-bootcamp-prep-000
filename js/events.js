2d33491887e713163c2c14bc35677273888d55a9
George Yudin <george.yudin@yahoo.com>
2017-09-12 21:46:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(e) {
+      alert("Hey!");
+    });
+}
+
+function frameIt() {
+  $('img').on("load", function(e) {
+      $(this).addClass('tasty');
+    });
+}
+
+function pressIt() {
+ $('#typing').on("keydown", function(e) {
+  if(parseInt(e.which) === 71) {
+   alert("Pressed G");
+  }
+ });
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+       alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
