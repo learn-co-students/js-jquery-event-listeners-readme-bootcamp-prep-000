bce3374e29134cd1b6d319d55cfd86eb8780a99e
Hailey Bobella <haileybobella@gmail.com>
2017-09-12 22:02:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
-//define functions here
+function getIt() {
+  $('p').on('click', () => {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', () => {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('form input').on('keydown', (key) => {
+    if (key.which == 71) {
+      alert('You pressed G!');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+
 
 $(document).ready(function(){
 
