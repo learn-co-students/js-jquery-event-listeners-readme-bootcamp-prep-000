81a1e15807b1814ca361ed9977561ae3e8f87297
David Kuruvilla <dapawn@gmail.com>
2017-09-12 14:55:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
+function getIt () {
+  $('p').on('click', function (){alert('Hey!')})
+}
+
+function frameIt () {
+  document.images[0].style.border = 'red';
+  document.images[0].className = 'tasty';
+}
+
+function pressIt () {
+  $('#typing').on('keydown', function (key){if (key.which === 71) {alert('The letter G was pressed.')}})
+}
+
+function submitIt () {
+  $("form").on("submit", function() {alert('Your form is going to be submitted now.')})
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
