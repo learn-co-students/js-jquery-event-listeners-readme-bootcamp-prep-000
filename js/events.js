bf8f2d9733e5c9b3e31950e4f648d47b45635646
Anthony Fields <janthonyfields@gmail.com>
2017-09-13 03:26:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(e) {
+    this.classList.add('tasty');
+    // this.className = 'tasty';
+    console.log(e)
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    // let e = arguments[0];
+    if ( parseInt(e.which) === 71 ) {
+      alert("g was pressed");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.");
+    e.preventDefault();
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
