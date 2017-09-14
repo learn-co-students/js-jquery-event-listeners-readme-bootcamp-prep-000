cad84ad2e560d8784753be7f381d19b17f22d7e9
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-09-14 04:10:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt(){
+  $('body').on('keydown', function(e){
+    if(e.which === 71){
+      alert('You pressed it!')
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+  alert("Your form is going to be submitted now.");
+  return;
+});
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  frameIt()
+  getIt()
+  pressIt()
+  submitIt()
 });
