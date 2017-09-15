c3df692cc3064d98c289909604a13a997f6aec09
Olena Ageyeva <eageeva@hotmail.com>
2017-09-15 19:48:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,30 @@
 //define functions here
 
+function frameIt(){
+  $("img").on("load",function(){
+  //  $("img").style.border='2x solid red'
+    $(this).addClass("tasty")
+  })
+}
+
+
+function getIt(){
+  $("p").on("click", function(){alert("Hey!")})
+}
+
+
+function submitIt(){
+  $("form").on("submit", function(){alert("Your form is going to be submitted now.")})
+}
+
+function pressIt(){
+  $("form").on("keydown", function(key){
+    if(key.which == 71){
+      alert('g was pressed');
+  }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
