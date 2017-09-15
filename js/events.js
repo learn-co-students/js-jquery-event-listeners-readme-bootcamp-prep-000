30503f9eec0b9fcd177e6d07af207557544c3807
Ion Gorincioi <gorincioii@yahoo.com>
2017-09-15 03:40:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
-//define functions here
+function getIt() {
+  $('p').click(function() {
+    alert('Hey!');
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+    return;
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('You prest -G- !!!');
+      return;
+    }
+    return;
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+
+frameIt();
+
+pressIt();
+
+submitIt();
 
 });
