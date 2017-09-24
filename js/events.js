7dff876d79b875726556ba126686b4dd081c99c8
Luke Borsare <lborsare1@gmail.com>
2017-09-24 17:45:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt(){
+  $('p').on("click",function() {
+    alert("Hey!")
+  });
+}
+function frameIt(){
+  $('img').on("load",function(){
+    $('img').addClass('tasty')
+  });
+}
+function pressIt(){
+  $(document).on("keydown", function(key){
+    if (key.which == 71 ) {
+      alert("You pressed G!")
+    }
+  });
+}
+function submitIt(){
+  $("form").on("submit",function(){
+      alert('Your form is going to be submitted now.');
+      return;
+  })
+}
 
 $(document).ready(function(){
-
+  window.getIt()
+  window.frameIt()
+  window.pressIt()
+  window.submitIt()
 // call functions here
 
 });
