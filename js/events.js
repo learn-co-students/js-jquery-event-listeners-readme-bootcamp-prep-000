7b453b5f381af1b31d69627408271650dc2bb8ce
Sandy Yeung <sandynism@gmail.com>
2017-09-30 22:54:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,9 +1,38 @@
-function getIt() {
-  
-}//define functions here
-
 $(document).ready(function(){
 
-// call functions here
+/function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 });
+
+```$(document).ready(function(){
+  // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+
+});```
