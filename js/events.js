225e19bd442b1fd34192c024a250ed1fa0948389
Adam Clayman <adam@clayman.org>
2017-09-30 15:37:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!');
+  });
+}
+function frameIt() {
+  $('img').on('load', function () {
+    $('img').append("class='tasty'");
+  });
+}
+function pressIt() {
+  $('#typing').on('keydown', function () {
+    if (input.which === 'G') {
+      alert('You entered a G!');
+    }
+  });
+}
+function submitIt() {
+  $('form').on('submit', function () {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 
 $(document).ready(function(){
-
 // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
