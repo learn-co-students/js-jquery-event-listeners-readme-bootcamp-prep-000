a94d457de9dbcd43ef97db9bbae93fc6a3d097d9
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-03 18:00:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")//action you want taken
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+  $('img').addClass('tasty');  //action you want taken
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+      alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if (e.which === 71) {
+    alert("G Key!")
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+submitIt()
+pressIt()
 });
