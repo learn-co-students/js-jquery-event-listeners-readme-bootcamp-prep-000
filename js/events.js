0088849da9b95e65e898c098910bf8e97a993b56
Simcha Wood <truthfulthomas@gmail.com>
2017-10-09 20:58:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').load(function() {
+    $(this).addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71)
+      alert("That's a G!");
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
