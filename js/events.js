a2f66d4b127f5bde5065384db61e03b8656851eb
Franco Aiello <steed430@hotmail.com>
2017-10-15 11:40:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+
+
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function pressIt(){
+  $("input").on("keydown", function(key){
+    if(key.which===71){
+      alert("You pressed G!!")
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit",function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function frameIt(){
+  
+  $("img").on("load", function(){
+    $("img").addClass("tasty")
+  })
+}
 
 $(document).ready(function(){
-
+getIt()
+pressIt()
+submitIt()
+frameIt()
 // call functions here
 
 });
