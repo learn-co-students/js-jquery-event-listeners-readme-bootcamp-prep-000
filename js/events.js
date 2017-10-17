65de1f1020c07cd0e02a4545536a1a4b9d8b41d1
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-17 15:18:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', () => alert('Hey!'));
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').load(function () {
+    $(this).addClass('tasty')
+  })
+}
 
-// call functions here
+function pressIt() {
+  $('input#typing').keydown(function (e) {
+    if (e.key == 'g' || e.which == 71) {
+      console.log(e);
+      alert("You have pressed the G key");
+    }
+  })
+}
 
+function submitIt() {
+  $('form').submit(function (e) {
+    e.preventDefault();
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+$(document).ready(function () {
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
