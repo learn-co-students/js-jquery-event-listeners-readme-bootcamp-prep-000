45c642e1f86c38189535450b63e1e1e7213ff9d7
Katy Carr <khcarr9@gmail.com>
2017-10-26 17:42:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  window.alert("Hey!");
+}
+
+
+function frameIt() {
+  $('img').on('load',function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which==71) {
+      window.alert("You pressed G")
+    }
+  })
+}
+
+function submitIt() {
+  window.alert("Your form is going to be submitted now.")
+}
 
 $(document).ready(function(){
-
 // call functions here
+$('p').on('click',getIt);
+
+frameIt();
+
+pressIt();
+
+$('form').on('submit',submitIt);
 
 });
