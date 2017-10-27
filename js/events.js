01a95865f90e9985c00f34fd0247bf69acabc436
Dawn Beattie <heartandhandstraining@gmail.com>
2017-10-27 04:41:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
 
-$(document).ready(function(){
+function frameIt(){
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  });
+}
 
-// call functions here
+funcion pressIt(){
+  $('input').on("keypress", function() {
+    if(key.which == 71) {
+      alert("G was pressed")
+    });
+}
 
+function submiIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  });
+}
+$(document).ready(func
+  tion(){
+    // call functions here
+    getIt();
+    frameIt();
+    pressIt();
+    SubmitIt();
 });
