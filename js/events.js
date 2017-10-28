565388eb5a2580dfe45dba93c2a8e973f71e4f2c
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-28 01:11:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
+function getIt(){
+  alert("Hey")
+}
+
+function frameIt(){
+$("img").addClass("tasty")
+}
+
+function pressIt(a){
+  console.log(a.which)
+  if ($('#typing').val() === "G"){
+    alert("G key was pressed down")
+  }
+}
+
+function submitIt(){
+  alert("Your form is going to be submitted now.")
+}
 //define functions here
 
 $(document).ready(function(){
-
+$('p').on('click', getIt)
+$('img').on('load', frameIt)
+$('#typing').on('keydown', pressIt)
+$('form:nth-child(2)').on('submit', submitIt)
 // call functions here
-
 });
