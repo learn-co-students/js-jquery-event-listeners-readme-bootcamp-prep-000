b954a1dff1fd54761979b44b57262a65bcb3bf37
Domnik Mack <domnikmack@gmail.com>
2017-10-30 15:28:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -21,8 +21,8 @@ function frameIt() {
 // Define a function pressIt that does not accept a parameter.
 // The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
 function pressIt() {
-  $("input").on("keydown", function(event) {
-    if(event.which === 103){
+  $("input").on("keydown", function(key) {
+    if(key.which === 71){
       alert("You have pressed the G key!");
     }
   });
