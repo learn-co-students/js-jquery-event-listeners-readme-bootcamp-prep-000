5d7ca7cf746d66891ece38ee610ae27bc61f3256
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 15:51:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $(window).on("load", function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $(document).on("keydown", function(e) {
+    if(e.which ==  71) {
+      alert("G");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
