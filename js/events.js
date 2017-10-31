cb58db19db3ea2529e5a98d49bdec28c415c028f
Leslie Phifer <atheneith@gmail.com>
2017-10-31 23:42:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
 //define functions here
+function getIt(){
+  $('p').on('click', _ => alert('Hey!'));
+}
+
+function frameIt(){
+  $('img').on('load', _ => $('img').addClass('tasty'));
+}
+
+function submitIt(){
+  $('form').on('submit', _ => alert('Your form is going to be submitted now.'));
+}
+
+function pressIt(){
+  $('input').on('keydown', e => {
+    if (e.which == 71) alert('You pressed the G button');
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
 
 });
