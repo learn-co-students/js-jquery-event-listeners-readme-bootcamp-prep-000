c2e6c4a3a15dfb89c7e4bc0934f637bda4b2d8ce
Christian Ahn <kwanghoonahn@gmail.com>
2017-11-02 20:33:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function(){
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+  return;
+}
+
+function pressIt() {
+  $("form").on("keydown", function(e){
+    if (e.which === "G".charCodeAt(0)) {
+      alert("You pressed the G key.");
+    }
+  });
+  return;
+}
+
+function submitIt() {
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+  return;
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+  
 });
