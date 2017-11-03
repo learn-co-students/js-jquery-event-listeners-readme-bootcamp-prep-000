afaa85a0cb2c99b690cbe1bbe230c2f2d30ff446
Eric Callari <ericcallari1@gmail.com>
2017-11-03 23:42:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+        $("img").addClass("tasty");
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71){
+      alert("A G don't come for free");
+    }
+  });
+}
+
+function submitIt() {
+  alert('Your form is going to be submitted now.');
+}
 
 $(document).ready(function(){
 
-// call functions here
+  $('p').click(function() {
+    getIt();
+  });
+
+  $('img').on('load', function(){
+    frameIt();
+  });
+
+  $("form").on("submit", function() {
+      submitIt();
+  });
 
 });
