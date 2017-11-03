92f12dde187d649f7c9da44cada05a52027e28ed
Magnus Skonberg <magnus.skonberg@gmail.com>
2017-11-03 15:50:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,25 @@
 //define functions here
-
+function getIt(){
+  $('p').on("click", function(){alert("Hey!");});
+}
+function frameIt(){
+  $('img').on('load', function(){
+    //adds the class`tasty` to the image to add a red frame to the image.
+    $('img').addClass('tasty')
+  });
+}
+function pressIt(){
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
+function submitIt(){
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+});
+}
 $(document).ready(function(){
 
 // call functions here
