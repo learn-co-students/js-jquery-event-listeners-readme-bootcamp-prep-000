10d3f9f0d69815ac0ed4ac828476d3da31a59182
John Heeg <fireman010203@gmail.com>
2017-11-04 18:30:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on("click", function() {
+    $('img').addClass("tasty");
+    //add tasty class and frame red
+  })
+}
+
+function pressIt() {
+  $('form').on("keydown", function(e) {
+    if(e.which === 71) {
+      alert("Shit you've pressed the fucking 'g', G!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+}
+
 
 $(document).ready(function(){
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 // call functions here
-
 });
