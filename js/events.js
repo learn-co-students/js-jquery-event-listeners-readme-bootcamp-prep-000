9b9d27d3313ac0174c668c0b726c0a16a4d1788d
Obi <obinnawalker@gmail.com>
2017-11-07 22:13:44 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
+
 //define functions here
+
+function getIt(){
+  $('p').on('click', function(){
+    alert('hey')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass("tasty");
+  })
+}
+
+
+function pressIt(){
+    $("input[type='text']").on('keydown', function(key){
+      if(key.which === 71)
+        alert("Hey");
+    })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submit();
 
 });
