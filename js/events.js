252670f27dc7dc639b2e2ff8a6af70942b75393d
Eduardo Magalhaes <ed83isme@gmail.com>
2017-11-17 16:57:04 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function(){
+    $(this).addClass("tasty")
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(e){
+    var key = e.which||e.keyCode;
+    if(key==71) alert("You have found my G spot.");
+  });
+}
+
+function submitIt() {
+   $("form").on("submit", function(){
+     alert("Your form is going to be submitted now.");
+   });
+}
 
 $(document).ready(function(){
 
