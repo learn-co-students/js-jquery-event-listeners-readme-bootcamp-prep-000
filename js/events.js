44843da3a9d799d73ffe126b92c11a6015bcb218
Luke Olsen <lukeaolsen@gmail.com>
2017-11-18 17:02:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+};
+
+function frameIt(){
+  $('img').load(function(){
+    $('img').addClass('tasty').css("border", "solid 2px red");
+  })
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if(key.which == 71){
+      alert("You have pressed the G key.");
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt();
+
+frameIt();
+
+submitIt();
+
+pressIt();
 
 });
