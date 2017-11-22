6aceb2b0094e5c46fc0feec3ddd4627efb095884
Max Zelinka <fourthwalljoke@gmail.com>
2017-11-22 19:32:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("hey!")
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
 
-// call functions here
+function submitIt() {
+  $('form').on('submit', function() {
+      alert("Your form is going to be submitted now.")
+
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key){
+    if(key.which == 71) {
+      alert("G was pressed")
+    }
+  })
+}
+
+$(document).ready(function() {
+
+getIt()
+frameIt()
+submitIt()
+pressIt()
 
 });
