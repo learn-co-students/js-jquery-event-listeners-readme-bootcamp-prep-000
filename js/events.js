106259c1d0d0e4522785da89ad86004cc3398280
Daniel Lai <dlai813@gmail.com>
2017-11-26 01:16:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
+function getIt() {
+    alert("Hey!");
+  }
+
+function frameIt() {
+    $('img').addClass("tasty");
+}
+
+function pressIt() {
+  $("input").on("keydown", function(e){
+    var key = e.which||e.keyCode;
+    if(key==71) alert("You have found my G spot.");
+  });
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
 
 $(document).ready(function(){
 
+$('p').click(getIt());
+
+$('img').load(frameIt());
+
+$('input').on('keydown', pressIt());
+
+$('form').on('submit', submitIt());
 // call functions here
 
 });
