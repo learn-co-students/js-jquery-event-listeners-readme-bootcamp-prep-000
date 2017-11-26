60b4824a38b2bfda6ca58a40681a715cf79b8b9f
Karen Zhao <karen.zhao18@gmail.com>
2017-11-26 21:44:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
-
-$(document).ready(function(){
+var getIt = function(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+};
+getIt();
+// $(document).ready(function(){
 
 // call functions here
 
-});
+var frameIt = function(){
+  $('.tasty').on('load', function(){
+    append("border: 10px solid; border-color: red;");
+  });
+};
+frameIt();
+
+var pressIt = function(){
+  $(document).on('keydown', function(key){
+    if(key.which == 71) {
+      alert("You pressed G");
+    }
+  });
+};
+pressIt();
+
+var submitIt = function(){
+  $('form').on('submit',function(){
+    // if ($("input:first").val() === "correct") {
+      alert("Your form is going to be submitted now.");
+      return;
+    // }
+    // alert("You entered the wrong value");
+    // return;
+  });
+};
+submitIt();
