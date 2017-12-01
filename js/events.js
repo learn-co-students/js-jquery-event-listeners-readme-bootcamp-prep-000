a967fddbcc4d8c43e62597b2d775362183e5ad72
Jason Lim <jayugc@gmail.com>
2017-12-01 21:15:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,29 @@
+function getIt() {
+  $('p').on("click", function() {
+    alert('"Hey!"');
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('form').on("keydown", function(key) {
+    if (key.which === 71) {
+      alert('You have pressed "G".')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 //define functions here
 
 $(document).ready(function(){
