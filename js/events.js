af7281d1f660f1be335408618df3e6602c5019a8
Jacob Silver <jacobsilver2@mac.com>
2017-12-06 20:18:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key){
+    if(key.which === 70) {
+      console.log("You pressed the 'g' key");
+    }
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
 
 });
