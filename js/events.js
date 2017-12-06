ed34b417704ff3bf06e17da2b375996e98106360
Jacob Silver <jacobsilver2@mac.com>
2017-12-06 20:22:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(key){
-    if(key.which === 70) {
+    if(key.which === 71) {
       console.log("You pressed the 'g' key");
     }
   })
