0f7eb3c4b578ff361f96b40fa08782848e6bcfa2
Leanna Keene <leannackeene@gmail.com>
2017-12-11 13:59:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,43 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $("img").addClass("tasty")
+    $(this).css('border', "solid 2px red")
+    return
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+        alert('g was pressed')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+    return
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+  getIt()
 
-});
+  frameIt()
+
+  pressIt()
+
+  submitIt()
+
+})
