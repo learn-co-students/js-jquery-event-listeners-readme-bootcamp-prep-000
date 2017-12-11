cce37b2b19ceb81c9aa641190d5a23c0c28f4c6b
Joe Lim <xjlim@users.noreply.github.com>
2017-12-11 12:02:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,68 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  });
+}
 
-// call functions here
+function pressIt() {
+  $("input").on("keydown", function(e) {
+    if (e.which === 71) {
+      alert("G is pressed!");
+    }
+  });
+}
 
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+$(document).ready(function() {
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+});
+//define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(e) {
+    if (e.which === 71) {
+      alert("G is pressed!");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+$(document).ready(function() {
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
