00e4a368ef5161b39eda6c28d9c5111c433b7046
Sinclair Kinch <sinclairkinch@gmail.com>
2017-12-12 05:33:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(e){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').addClass("tasty");
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+    if (e.which === 71) alert("Hey!");
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(e){
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  //$('img').addClass("tasty");
+  pressIt();
+  submitIt();
 
 });
