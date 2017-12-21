81ef0e2953fc41e47ce80dbfb07ceb863cd2ba2b
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-21 04:20:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt(){
 
-$(document).ready(function(){
-
-// call functions here
-
+$(document).ready(function(getIt){
 });
+$('p').on('click', function(){
+  alert( "Hey" )}
+);
+}
+
+
+function frameIt(){
+  $(document).ready(function(frameIt){
+  });
+  $('img').on('load', function(){
+  $('img').addClass('tasty');
+});
+}
+
+function pressIt(){
+  $(document).ready(function(pressIt){
+  });
+  $('form').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
+
+function submitIt(){
+  $(document).ready(function(pressIt){
+  });
+  $( 'form' ).submit(function( event ) {
+  alert( 'Your form is going to be submitted now.' );
+});
+}
