8fbb5a18c0695aad67419cb8c2489750ca6b4cd6
Michelle B <mjblevins7@gmail.com>
2017-12-24 05:06:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(key) {
+    alert('Hey!');
+})
+}
+
+function frameIt() {
+  $('img').on('load', function(key) {
+    return $('img').addClass('tasty')
+})
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which == 71){
+     alert('G was pressed');
+    }
+})
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+      alert('Your form is going to be submitted now.');
+      return;
+  })
+}
+
+
 
 $(document).ready(function(){
 
 // call functions here
+return getIt();
+return frameIt();
+return pressIt();
+return submitIt();
 
 });
