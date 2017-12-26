9c46182c05841a146b962d989e7d3afc4c8603d7
Salma Elshahawy <salmaeng71@gmail.com>
2017-12-26 10:59:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function(){
+    $(this).addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $("#typing").on("keydown", function(e){
+    var key = e.which||e.keyCode;
+    if(key==71) alert("You have found my G spot.");
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
