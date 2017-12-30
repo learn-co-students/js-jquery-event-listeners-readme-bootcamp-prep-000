94342d62d5602d2ac924dc32991ebf236aa42241
Allyson Wesman <allysonwesman@gmail.com>
2017-12-30 23:15:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function () {
+    alert ("Hey!");
+  })
+  return;
+}
+
+function frameIt() {
+  $(window).on("load", function () {
+    $('img').addClass("tasty")
+  })
+  return;
+}
+
+function pressIt() {
+  $('form').on("keydown", function (key) {
+    if (key.which == 71) {
+      console.log(key.which);
+      alert ("You have pressed G");
+    }
+  })
+  return;
+}
+
+function submitIt() {
+  $('form').on("submit", function () {
+      alert ("Your form is going to be submitted now.");
+  })
+  return;
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+  frameIt();
+  getIt();
+  pressIt();
+  submitIt();
 
 });
