c6a3d09d107c7b985c47931aa50523d0f6a1f115
Topi Kettunen <topi.kettunen@myy.haaga-helia.fi>
2018-01-04 12:11:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt() {
+  alert("Hey!");
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  
+  $('input').on('keydown', function(key) {
+    if(key.which == 71){
+        alert('G was pressed');
+    }
+  });
+  
+}
+
+function submitIt() {
+  
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  });
+  
+}
+
 $(document).ready(function(){
-
-// call functions here
-
+  // call functions here
+  
+  $('p').on('click', getIt());
+  
+  $('img').on('load', frameIt());
+  
+  $('input').on('keydown', pressIt());
+  
+  $('form').on('submit', submitIt());
+  
 });
