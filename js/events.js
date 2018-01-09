50cb1c5bcafe07f1fc20f44ff722c74cff482736
Peter Coughlin <coughlin1966@gmail.com>
2018-01-09 16:19:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,10 +2,18 @@ function getIt() {
   alert('Hey!')
 }
 
+function frameIt() {
+  var pix = document.getElementsByTagName('img')
+  pix.style.border='2px solid red'
+}
+
 $(document).ready(function(){
 
 $(p).on('click', function() {
   getIt()
+})
+#(img).on('load' function() {
+  frameIt()
 })
 
 });
