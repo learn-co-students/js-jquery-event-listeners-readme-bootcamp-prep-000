ca845231665de692576ec91c3c80a14260820234
desmond beramendi <desmond7187@gmail.com>
2018-01-09 21:22:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,34 +2,37 @@
 function getIt() {
   $('p').on("click", function() {
     alert("Hey!");
-  })
+  });
 }
 
 function frameIt() {
   $('img').on("load", function() {
     $( this ).addClass('tasty');
-  })
+  });
 }
 
 function pressIt() {
   $('form input:first-child').on("keydown", function(key) {
     if(key.which === 71 || key.detail === 71) {
-      alert("You pressed G!")
+      alert("You pressed G!");
     }
-  })
+  });
 }
 
 function submitIt() {
   $('form:last').on("submit", function() {
     
     
-    alert("Your form is going to be submitted now.")
-  })
+    alert("Your form is going to be submitted now.");
+  });
 }
  
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
