1b42aa3ffc8d3c5eecb72f377c4adb026fb7f659
Harim Choi <harim1206@gmail.com>
2018-01-26 16:45:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,55 @@
 //define functions here
 
+
+function getIt(){
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt(){
+  console.log('call')
+  $('img').on('load', function(){
+    $(this).addClass("tasty");
+
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if (e.location === 71){
+      alert ('pressed g!');
+    }
+  })
+}
+
+function submitIt(){
+
+  $('form').on('submit',function(){
+    alert ('Your form is going to be submitted now.');
+  })
+
+}
+
+/*
+
+Define a function submitIt that does not accept a parameter.
+ The function should bind a submit event to the form
+ that alerts "Your form is going to be submitted now.".
+
+
+*/
+
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
+
 
 });
