573db4b7326df7710f54e9270b6f7732dc3c0927
Aspen James <aspenjames@tqca.org>
2018-01-30 04:24:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("#typing").on("keydown", function(e){
+    if(parseInt(e.which) == 71){
+      alert("You have pressed G.");
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
