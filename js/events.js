637dd4cbd4771ad8a05d869a73b14eba7b1d5602
Thomas Casady <tscasady@gmail.com>
2018-02-22 19:55:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if (key.which === 71){
+      alert('g has been pressed')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function (){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 
 $(document).ready(function(){
-
+getIt()
 // call functions here
 
 });
