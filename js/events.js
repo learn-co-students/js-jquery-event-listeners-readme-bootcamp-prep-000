0a61fe129af3ffd3b5cde8ed2291f6e10fda6f52
Jesse Billard <jessebillard@mac.com>
2018-02-27 20:41:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
+//get it got it good
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+//we bumpin'
+function frameIt() {
+  $('img').on('load', function(){
+    var img = document.querySelector('img');
+    img.className = 'tasty';
+    img.style.border = 'red';
+  })
+}
+
+//sleigh bae
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which === 71 ) {
+      alert('You have pressed the G Thang!')
+      return;
+    }
+  })
+}
+
+//submission complete
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
 
+  frameIt();
+  
+  pressIt();
+  
+  submitIt();
 });
