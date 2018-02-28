8cfcb36c69d8c6f59b9c832ce9bd34c626a0e7ea
Pitta <mailez4all@gmail.com>
2018-02-28 10:40:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,49 @@
 //define functions here
+function getIt() {
+$('p').on("click", function() {
 
-$(document).ready(function(){
+  alert("Hey!");
+  return;
+});
+}
 
+function frameIt() {
+
+  $('img').on("load", function() {
+
+    $('img').addClass("tasty");
+
+  });
+}
+
+function pressIt() {
+  var key = 71;
+  $('form').on('keydown', function(key) {
+
+    if (key.which == 71) {
+      alert('G was pressed')
+      return;
+    }
+  });
+
+}
+
+function submitIt() {
+
+  $('form').on('submit', function() {
+
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+
+}
+
+
+$(document).ready(function() {
+
+getIt();
+
+pressIt();
 // call functions here
 
 });
