e2fe48b90349fab04594dba5091b7b8cb57e0858
Kelsey Fecho <kfecho30@gmail.com>
2018-02-28 01:58:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  })
+}
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  })
+}
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which === 71){
+      alert('You have pressed the G key.');
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
