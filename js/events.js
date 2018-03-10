f00afaaeee44198b52d642cc13edfa02a5c34eac
maxsmouha@gmail.com <maxsmouha@gmail.com>
2018-03-10 23:16:06 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt () {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+$('img').on('load', function(){
+      .addCLass('tasty')
+  });
+}
+
+function pressIt() {
+$('img').on('keydown', function(key){
+  if (key.which == 71){
+      alert('Hey!')
+    }
+  });
+}
+
+function submitIt() {
+
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+
+frameIt()
+
+pressIt()
 
 });
