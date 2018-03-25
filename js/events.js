6435fcfb37580bdd73577641593cdc1eaebd2ed2
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-25 05:16:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,14 +1,36 @@
 //define functions here
 
 function getIt() {
-  $(document).on('click', function() {
+  $('p').on('click', function() {
     alert("Hey!");
   });
 }
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass("tasty");
+  });
+}
 
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which == 71){
+      alert('G was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(key) {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+$(document).ready(function(){
 // call functions here
   getIt();
-
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
