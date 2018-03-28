779c4454cb43a720e2f68bd46b5f459a51de21eb
Jacob Berman <jacobberman1995@gmail.com>
2018-03-28 20:59:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
+
+function getIt(){
+  $("p").on('click',function(){
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $("img").on('load',function(){
+    var element;
+    element = $("img")
+    element.addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("form").on('keydown', function(key){
+    if (key.which == 71){
+      alert("you hit g")
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
