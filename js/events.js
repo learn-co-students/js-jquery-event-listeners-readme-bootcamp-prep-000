461da54334f5ab43f3d4ab887ffb56979c9ddb71
Erlliam Mejia <tommejas@gmail.com>
2018-04-04 23:40:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
+
+function getIt() {
+  $("p").on("click", function(event) {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function(event) {
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(event) {
+    if (parseInt(event.which) === 71) {
+      alert("you pressed g");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").submit(function(event) {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
