4d11658c166f3ef2d17fc5394fcd80f73ddf21f8
Ekaterina Rodkina <katrinrodkina@gmail.com>
2018-04-11 00:55:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(key) {
+    alert('Hey!');
+});
+  
+}
+// The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
+function frameIt (){
+    $('img').on('load', function(key) {
+     $('img').addClass('tasty')
+});
+  
+}
+//The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
+function pressIt () {
+  $('input').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+  
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+
+    alert('Your form is going to be submitted now.');
+
+});
+}
+
+
+
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
