af16941436013d12b7de155a6ab50d18cb415f70
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-13 23:27:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
-$(document).ready(function(){
-
-// call functions here
-
-});
+  function getIt() {
+  $('p').on("click", function() {
+    alert("Hey")
+    })
+  }
+  
+  function frameIt() {
+    $("img").on("load", function() {
+      $("img").addClass("tasty")
+    })
+  }
+  
+  function pressIt() {
+    $('input').on("keydown", function(g) {
+      const key = parseInt(g.which || g.detail)
+        if (key === 71) {
+          alert('g was pressed')
+        } 
+    })
+  }
+  
+  function submitIt() {
+    $("form").on("submit", function() {
+      alert("Your form is going to be submitted now.")
+    })
+  }
+  
+$(document).ready(function() {
+  getIt()
+  
+  pressIt()
+  
+  submitIt()
+  
+  frameIt()
+}) 
