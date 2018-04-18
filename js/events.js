da9b7cb100da8fedf03914725c83cc4169e526e6
Lorena <lorena.giddens@outlook.com>
2018-04-18 06:24:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,35 @@
 //define functions here
 
+/* The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!". */
+function getIt() {
+  $('p').on("click", function(){
+    alert('Hey!');
+  });
+}
+
+/* The function should bind the load event that adds the class tasty to the image to add a red frame to the image. */
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+/* The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key. */
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  });
+}
+
+/* The function should bind a submit event to the form that alerts "Your form is going to be submitted now.". */
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
