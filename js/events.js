d995931c4266117fde0edfcbd0b1d7d1ed538627
Emily Jirles <emily.jirles@gmail.com>
2018-04-20 18:55:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
+// define functions here
+
+function getIt(){
+  return $('p').on('click', function(){ 
+    alert('Hey!');
+    return;
+    }
+  );
+}
+
+function frameIt(){
+  return $('img').on('load', function(){
+    return $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  return $('form').on('keydown', function(e){
+    if (e.which === 71){
+      alert("'g' has been pressed");
+      return;
+    }
+  });
+}
+
+function submitIt(){
+  return $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
