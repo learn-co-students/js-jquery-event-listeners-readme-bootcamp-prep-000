58f4c414a3750b7a0dcece9ce9949fc1e85ce17f
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-04-23 20:20:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,19 @@
-//define functions here
+
+function getIt(){
+  $('p').on("click", function(){
+  alert ("Hey!")   
+  })
+  
+  function frameIt(){
+  $('tasty').on("load", function(){})
+  }
+  
+  
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
 
 });
