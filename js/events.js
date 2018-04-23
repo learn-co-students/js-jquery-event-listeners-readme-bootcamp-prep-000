e99d28695241e13d6a1c19f92a9bb2ac6d216c04
Jigar Polra <jigarpolra@live.co.uk>
2018-04-23 16:07:44 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert("Hey");
+  });
+}
+
+function frameIt() {
+  $('body img').on('load', function(e) {
+    $('body img').attr('class', "tasty");
+  });
+}
+
+function pressIt() {
+  $('form input').on('keydown', function(e) {
+  if(parseInt(e.which) == 71)
+  {
+    alert("You have pressed G");
+  }
+});
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
 
 });
