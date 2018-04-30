ba28ea6aa5b614115cbefc8c0c5e3aecee98621a
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-30 23:57:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
 
+function getIt() {
+ $('p').on("click",function() {
+   alert("Hey!");
+ })
+}
+
+function frameIt() {
+  $('img').on('load',function() {
+    return $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $("form").on("submit",function() {
+    if($('input' === "G")) {
+      alert("you entered G");
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
 
 });
+
+
