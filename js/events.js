90dfef4c76b3722206bd1787c38ce4b1c7568d8e
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-02 17:23:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
+function getIt(){
+  $('p').on('click', () =>{alert('Hey!'); return;});
+}
+
+function frameIt(){
+  $('img').on('load', () =>{this.classList.add('tasty'); return;});
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+    e === 71 ? alert('Pressed G!') : null;
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
