fe512e43ef5f2515f98e668e3f9ed3234339b3cd
Jed Peek <jedpeek@yahoo.com>
2018-05-02 22:02:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,27 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+};
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+};
+function submitIt(){
+  $("form:last").on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+};
+
+function pressIt(){
+  $("form:first").on("keydown", function(e){
+    if(parseInt(e.which) === 71){
+      alert("G was pressed!")
+    }//end if
+  })//end jquery event
+}//end function
+
 
 $(document).ready(function(){
 
