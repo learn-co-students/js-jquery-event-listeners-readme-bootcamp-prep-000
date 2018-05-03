f7d8efbd4a7d9babbf7b96bf41474c67c21b35f7
Mendy <mendybeltcohen@gmail.com>
2018-05-03 19:05:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,6 +6,26 @@ $(document).ready(function(){
 
 });
 
-$('p').on('eventname', function getIt(){
+function getIt() {
+  $('p').click('eventname');
   alert('Hey!');
-});
+}
+  
+
+function frameIt() {
+  $('img').addClass('tasty', 'load');
+  
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e){
+    if(e.which == 71){
+    alert();
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit');
+  alert("Your form is going to be submitted now.");
+}
