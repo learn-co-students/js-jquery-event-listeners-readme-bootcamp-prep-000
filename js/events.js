bd5eb21bf817e5f021c34073c9e0b1e2fe4acb44
J Daniel <jdaniel1008@gmail.com>
2018-05-06 21:53:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt(){
+  alert('Hey!')
+}
+
+function frameIt(){
+  $('img')[0].className = "tasty"
+}
+
+function pressIt(){
+  $('#typing').on("keydown",function(event){
+    if (event.which === 71) {
+      alert("G")
+    }
+  })
+}
+
+function submitIt(){
+  alert("Your form is going to be submitted now.")
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+$('p').on("click",getIt);
+$('img').on("load",frameIt);
+$('#typing').on("keydown",pressIt);
+$('form').on("submit",submitIt);
 });
