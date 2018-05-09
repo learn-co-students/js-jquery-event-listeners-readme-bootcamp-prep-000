b155ce8150b23cce2b623b998507a33b99657c74
Adrian Samarripas <asamarripas@gmail.com>
2018-05-09 01:14:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,36 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  return alert("Hey!");
+}
+
+$('p').on('click', function getIt(){
+    return alert("Hey!");
+})
+
+
+function frameIt(){
+   $('img').addClass('tasty');
+}
+
+$('img').on('load', function frameIt(){
+  $('img').addClass('tasty');
+})
+
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if(key.which === 71){
+      alert("You're a G!");
+    }
+  });
+}
+
+
+function submitIt(){
+  $('form').on('submit', function(){
+    return alert("Your form is going to be submitted now.")
+  })
+}
