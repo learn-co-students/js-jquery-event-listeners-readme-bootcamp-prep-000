9dfc3899f8ff9fd0a86f4dad83b39218aa610ee8
Jason Pitts <juniorjr083@gmail.com>
2018-05-14 03:22:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,56 @@
 $(document).ready(function(){
 
 // call functions here
+frameIt();
+getIt();
+pressIt();
+submitIt();
+
+
 
 });
+
+
+function getIt(){
+  $('p').on('click', function(){
+  alert('Hey!');  
+    
+  });
+  
+  
+}
+
+function frameIt(){
+    $('img').on('load', function(){
+      $('img').addClass('tasty');
+    });
+    
+  }
+  
+  function submitIt(){
+    $('form').on('submit', function(){
+      alert("Your form is going to be submitted now.");
+      
+    });
+    
+  }
+  
+  function pressIt(){
+    $('input').on('keydown', function(k){
+      
+      if(k.which === 71){
+        alert("G pressed");
+        
+      }
+      else{
+        return false;
+      }
+      
+    });
+    
+    
+  }
+  
+  
+  
+  
