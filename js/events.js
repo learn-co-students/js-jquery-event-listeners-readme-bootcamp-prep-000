97ac58861aa1fba3d869eca425df2168f4b17792
Shianne Lee <shiannelee@live.ca>
2018-05-19 00:31:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  return $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+return $('img').on('load', function(){
+  $('img').addClass('tasty');
+});
+}
+
+function pressIt(){
+  return $(document).on('keydown', function(key){
+    if(key.which == 71){
+      alert('G key is pressed');
+    }
+  });
+}
+
+function submitIt(){
+return $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+});
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
