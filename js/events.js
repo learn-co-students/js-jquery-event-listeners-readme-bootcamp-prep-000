d5adad7970bc2045303171105466832c02eee36a
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-21 16:24:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,3 +1,29 @@
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71) {
+      alert("g was pressed")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+      alert('Your form is going to be submitted now.')
+  })
+}
+
 //define functions here
 
 $(document).ready(function(){
