7485660a78b31591ab9b45dbd9dc20ac05f83377
Nathan Hill <nateohill@gmail.com>
2018-05-21 18:00:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,24 @@
-//define functions here
+function getIt() {
+  $('p').click(alert('Hey!'));
+}
+
+function frameIt() {
+  $('img').on('load', function() { 
+    $('img').addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $('form').submit(alert("Your form is going to be submitted now."));
+}
+
+function pressIt() {
+  $('input').on('keydown', (key) => {
+    if(key.which === 71) {
+      alert("You clicked G");
+    }
+  });
+}
 
 $(document).ready(function(){
 
