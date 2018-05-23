cbddbdc637435459f1b36589a40e5273a15493cc
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-23 22:19:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt () {
+  $('p').on('click',function() {
+    alert('Hey!')
+  })
+}
 
-$(document).ready(function(){
+function frameIt () {
+  $('img').on('load', function (){
+    $('img').addClass('tasty')
+  })
+}
 
-// call functions here
+function pressIt () {
+  $('#typing').on('keydown', function(e) {
+    if (e.which===71) {
+      alert('you pressed g')
+    }
+  })
+}
+
+function submitIt () {
+  $('form').on('submit', function(e){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+$(document).ready(function() {
+   $('form').on('submit', function(e){
+    alert('Your form is going to be submitted now.')
+  })
+
 
 });
