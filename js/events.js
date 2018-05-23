d8a7b3abe66bc79799c4941e34d631144b17bb80
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-23 20:07:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,31 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function(){
+    return alert('Hey!')
+})
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+  return $('img').addClass("tasty")
+})
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('s was pressed');
+  }
+})
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
