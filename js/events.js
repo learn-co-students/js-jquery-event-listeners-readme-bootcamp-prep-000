c677814aeda224c679c866da265432de794ff5d4
Mahad Najam <mahad.najam15@gmail.com>
2018-05-24 05:58:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+   $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown", function(k){
+    if (k.which == 71){
+      alert("You pressed G.");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  });
+}
+
+function init(){
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  // call functions here
+  init();
 });
