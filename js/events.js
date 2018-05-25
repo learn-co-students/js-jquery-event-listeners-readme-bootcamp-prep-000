5db5d8491e987aef715c7acfb695f901c5729bfe
Steven Crouse <sdcrouse91@gmail.com>
2018-05-25 03:45:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,29 +2,50 @@
 function getIt() {
   $('p').on("click", function(){
     alert("Hey!");
-    //return;
   });
 }
 
-function frameIt() {
+function frameIt() { /* This will execute when it knows that the image has loaded, as is the case with mocha.run(). But without those tests (which wind up calling this function twice, I think, in order to set up stuff and check values), frameIt doesn't always know whether the image has been loaded. */
+
+  //console.log("Will this display?");
+  //$('img').trigger('load');
   $('img').on('load', function() {
-   //$('img').addClass( "tasty" );
-    //This works, too: 
-    document.getElementsByTagName('img')[0].className = "tasty";
-    // This does not: $('img').className = "tasty";
-    //return;
+    //console.log("Testing!");
+    
+    $('img').addClass("tasty");
+    
+   // console.log("This is a sentence.");
+    //This works, too: document.getElementsByTagName('img')[0].className = "tasty";
   });
 }
 
 function pressIt() {
-  $("form").on("keydown", function() {
+  $("form input:first").on("keydown", function(key) {
     
+    if(key.which == 71){
+      alert('g was pressed');
+    }
   });
 }
 
+/*function pressIt(){ //This doesn't work.
+  $('form').on('keydown', function() {
+    if($( 'input:first' ).val() == "g"){
+      alert("g was pressed");
+    }
+  });
+}*/
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert ("Your form is going to be submitted now.");
+  })
+}
+
 $(document).ready(function(){
-  console.log('jump1')
+  //console.log('jump1');
   frameIt();
   getIt();
-  console.log('jump')
+  pressIt();
+  //console.log('jump');
 });
