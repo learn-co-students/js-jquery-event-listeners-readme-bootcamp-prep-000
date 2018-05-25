59f3df85ba4c83963723c25e7b7e08fef379f306
Steven Crouse <sdcrouse91@gmail.com>
2018-05-25 01:36:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!");
+    //return;
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+   //$('img').addClass( "tasty" );
+    //This works, too: 
+    document.getElementsByTagName('img')[0].className = "tasty";
+    // This does not: $('img').className = "tasty";
+    //return;
+  });
+}
+
+function pressIt() {
+  $("form").on("keydown", function() {
+    
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  console.log('jump1')
+  frameIt();
+  getIt();
+  console.log('jump')
 });
