40fd2fdbbefac50a294abd833ee1ace878c855e9
Forrest Benton <elliottbenton@gmail.com>
2018-05-31 18:23:56 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').attr('class','tasty');
+}
+
+function pressIt() {
+  $('#typing').on('keydown',function(key){
+    if (key.which == 71) {
+      alert("You've pressed 'G'.");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
 });
