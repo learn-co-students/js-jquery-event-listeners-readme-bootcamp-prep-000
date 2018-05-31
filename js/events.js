edbb24e862f2d626e49f9d87c9a7e9c6f9241622
Maxim Dalton <maximcdalton@gmail.com>
2018-05-31 05:34:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
+var getIt = function() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+};
+
+var frameIt = function() {
+  $(document).on('load', function() {
+    $('img').addClass("tasty");
+  });
+};
+
+var pressIt = function() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("g was pressed!");
+    }
+  });
+};
+
+var submitIt = function() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+};
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
