2012f7331265f83bdbd58ea05bd8783aa572d9e8
Sean Huang <hyprul@ucla.edu>
2018-06-03 03:03:13 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -31,7 +31,7 @@ function pressIt(){
 
 function submitIt(){
 	$('form').on('submit', function(){
-			alert("Your form is going to be submitted now.")
+		alert("Your form is going to be submitted now.")
 	})
 
 }
