4a0d72f2d36d1d0e200f5f0ef41b20f0b4ca8a55
Trevor Jameson <tjameson15@gmail.com>
2018-06-03 01:01:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,36 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+    return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(e) {
+    $('img').style.borderStyle = "solid";
+    $('img').style.borderColor = "red";
+    return;
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if (e.key === 'G') {
+      alert("You have pressed the \'G\' key.");
+      return;
+    }
+  })
+}
+
+function submitIt() {
+  $('submit').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
