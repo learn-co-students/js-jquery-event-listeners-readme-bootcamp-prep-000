59cd73b4b3cb65945dbe7c8b3794762ecf66dd37
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-04 04:17:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,27 @@
 
 $(document).ready(function(){
 
-// call functions here
-
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!") });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty') });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.") });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert("G was pressed");
+  }
+});
+}
