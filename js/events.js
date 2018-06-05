4950d0f69e6c3bd79ca977835a95884caa6cce8d
Jess Jacobson <jessanova@gmail.com>
2018-06-05 19:29:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -4,4 +4,30 @@ $(document).ready(function(){
 
 // call functions here
 
+getIt()
+frameIt()
+pressIt()
+submitIt()
+
 });
+
+function getIt(){
+  $('p').on('click',function(){alert('Hey!')})
+}
+
+function frameIt(){
+  $('img').on('load',function(){$(this).addClass('tasty')})
+}
+
+function pressIt(){
+  $('#typing').on('keypress',function(evt){
+    console.log('evt='+evt+' and evt.which='+evt.which);
+    if(evt.which===71){
+      alert('G')
+      }
+    })
+}
+
+function submitIt(){
+  $('form').on('submit',function(){alert('Your form is going to be submitted now.')})
+}
