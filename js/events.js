5bb3c1221fc4e378d8b97203a401760936a1fe84
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-06 19:34:04 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
+const getIt = () => {
+  $('p').on('click', () => {
+    alert('Hey!')
+  })
+}
+
+const frameIt = () => {
+  $('img').on('load', () => {
+    $('img').addClass('tasty')
+  })  
+}
+
+const pressIt = () => {
+  $('input').on('keydown', (key) => {
+    if (key.which === 71) {
+      alert('G key has been pressed.')
+    }
+  })  
+}
+
+const submitIt = () => {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.')
+  })  
+}
 
 });
