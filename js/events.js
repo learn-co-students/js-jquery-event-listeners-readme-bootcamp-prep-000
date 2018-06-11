66bf3e3bc96cbb2436ad4fb64845674e32c21299
Alex Edwards <info@acedigitalagency.net>
2018-06-11 21:57:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+    <p>"Hey!"</p>
+});
+}
+function frameIt() {
+  $('img').on('load', function(){
+  //actions you want to happen
+});
+}
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+});
+}
+function submitIt() {
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+})
+}
+
 
 $(document).ready(function(){
-
 // call functions here
+  <p> This is some text to click</p>
+  getIt();
+
+  <img src="https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg">
 
 });
