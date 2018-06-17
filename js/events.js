61a29b99d0cf8e5b0a613cccf289ae435f176419
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-17 22:12:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,66 @@
 //define functions here
 
-$(document).ready(function(){
+//$(document).ready(function(){
+  
+  function getIt(){
+    $('p').on("click", function (){
+      
+    alert("Hey!");
+  
+    })
+  }
+  
+  function frameIt(){
+  $("img").on("load", function frameIt(){
+  $("img").addClass("tasty");
+  
+    
+  })
+  }
+  
+  function pressIt(){
+    $("input").on("keydown", function pressIt(e){
+  if (e.which ===71){
+    alert('yOU PRESSED g');
+  
+  }
+  })
+  }
+  function submitIt(){
+    $('form').on('submit', function submitIt(){
+  
+    alert("Your form is going to be submitted now.");
+  
+})
 
-// call functions here
+}
 
+
+
+//});
+ 
+ /*
+
+  
 });
+
+$("img").on("load", function frameIt(){
+  $("img").addClass("tasty");
+  
+ })
+
+$("input").on("keydown", function pressIt(e){
+  if (e.which ===71){
+    alert('yOU PRESSED g');
+  }
+ 
+});
+
+$('form').on('submit', function submitIt(){
+  
+  alert("Your form is going to be submittef now");
+  
+});*/
+
+
+
