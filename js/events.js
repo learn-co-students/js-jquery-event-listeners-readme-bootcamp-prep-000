9e0d5235f2cb3a9a7b2f0a3161f3af5d83c8c600
Alexandra Batrak <alexandrabatrak@gmail.com>
2018-06-24 17:58:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
-
+function getIt(){
+  $('p').on("click", function(){
+    alert('Hey!')
+  })
+}
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('input').on("keydown", function(e){
+    if (e.which === 71){
+      window.alert('You pressed "G".')}
+  })
+  return;
+}
+function submitIt(){
+  $('form').on("submit", function(){
+    window.alert('Your form is going to be submitted now.');
+    return;
+  })
+}
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
