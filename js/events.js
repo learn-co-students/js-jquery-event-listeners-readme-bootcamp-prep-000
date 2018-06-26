166516ca8a9d8d0ebcb0ce701b21d6a8402818d0
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-26 10:48:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function frameIt()
+  {
+    $('img').on('load', function(){
+      $('img').addClass('tasty');
+    });
+  }
+  
+  function getIt()
+  {
+    $('p').on('click', function(){
+      alert('Hey!');
+    })
+  }
+
+function pressIt()
+{
+  $('form input:first-child').on('keydown', function(key){
+    if(key.which === 71)
+    {
+      alert("You've pressed the G key");
+    }
+  });
+}
+
+function submitIt()
+{
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
