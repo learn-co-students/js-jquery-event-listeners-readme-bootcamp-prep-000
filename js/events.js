03ece6336a05d9a725dae9743bfaa013ca1c04a6
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-03 10:08:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,42 @@
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  
+  frameIt();
+  
+  pressIt();
+  
+  submitIt();
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('body img').on('load', function() {
+    $("body img").addClass("tasty");
+    console.log('Trying to add the class tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    console.log(`Key pressed = ${key.which}`)
+    if(key.which == 71) {
+      alert('You have pressed the G key!!');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert('Your form is going to be submitted now.');
+    console.log('Submitting form');
+    return;
+  })
+}
