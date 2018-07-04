3db0eaf08a510cd838b33dd1abfdc02b2c1ec2e2
Derrick Skipwith <dskipwith15@gmail.com>
2018-07-04 15:57:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,32 +5,42 @@ function getIt(){
 
 
 
-  function frameIt(){
-    $('img').on('load', function(){
-      $(this).addClass('tasty');
-    });
-  }
+  getIt();
 
-  function pressIt(){
-    $('form input').on('keydown', function(key){
-      if(key.which == 71) alert("G was pressed");
-    });
-  }
+  frameIt();
 
-  function submitIt(){
-    $('form').on('submit', function(){
-      alert("Your form is going to be submitted now.");
-    });
-  }
+  pressIt();
 
-  $(document).ready(function(){
-    getIt();
-    frameIt();
-    pressIt();
-    submitIt();
-  });
-
-
-// call functions here
+  submitIt();
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('body img').on('load', function() {
+    $("body img").addClass("tasty");
+    console.log('Trying to add the class tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    console.log(`Key pressed = ${key.which}`)
+    if(key.which == 71) {
+      alert('You have pressed the G key!!');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert('Your form is going to be submitted now.');
+    console.log('Submitting form');
+    return;
+  })
+}
