d8ccde84b87918f64e388f4eb262603e8f1a0122
Felix Chan <felixchan29@gmail.com>
2018-07-05 16:41:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -4,4 +4,36 @@ $(document).ready(function(){
 
 // call functions here
 
+
 });
+
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!")
+  });
+  return;
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass("tasty");
+  });
+  return;
+}
+
+function pressIt(){
+  $(document).on("keydown", function(key){
+    if(key.which == 71){
+      alert("G was pressed");
+    }
+  });
+  return;
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+  return;
+}
+
