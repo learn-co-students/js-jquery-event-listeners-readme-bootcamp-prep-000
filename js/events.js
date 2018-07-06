77785bdfd0236a189bc902932ce7e299031e2d6b
Lucas Kabot <loyd.kim14@gmail.com>
2018-07-06 03:00:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    //adds the class "tasty" to the image
+    
+    $("img").addClass( "tasty" )
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert("You've pressed the 'G' key!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+  
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
