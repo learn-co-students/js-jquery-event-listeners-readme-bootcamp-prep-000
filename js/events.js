2c98b823e0dbd131c72e3f4c883e1838bd35c5fe
Brad Schrag <brad.schrag@gmail.com>
2018-07-13 18:32:35 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,47 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+    return
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  }).each(function(){
+    if(this.complete) {
+      $(this).trigger('load');
+    }
+  })
+}
+
+function pressIt() {
+  $('input#typing').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('g pressed')
+      return
+    }
+    return
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+    return
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+
+frameIt();
+getIt();
+pressIt();
+submitIt();
 
 });
