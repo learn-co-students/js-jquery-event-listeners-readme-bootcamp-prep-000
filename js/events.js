fa785fde7630534dca3d65fab0e7662f6888d0a6
Viktor Ivanov <sessgreem@gmail.com>
2018-07-15 19:16:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,31 +1,27 @@
 function getIt(){
   return $('p').on('click',function(){
     window.alert('Hey!')
-    return;
   })
 }
 
 function frameIt(){
   return $('img').on('load',function(){
     $('img').addClass('tasty');
-  return;
   })
 }
 
 function pressIt(){
-  return $('input').on('keydown',function(e){
+  return $('#typing').on('keydown',function(e){
     if(e.which === 71){
       window.alert("You've pressed G key!")
-      return;
     }
   })
 }
 
 function submitIt(){
-  return $('body').on('submit',function(){
+  return $('form').on('submit',function(){
     window.alert('Your form is going to be submitted now.')
-    return;
-  })
+ })
 }
 
 
