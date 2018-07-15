de59c5507f5a552cf05a7ec9bf1c604f49f278a4
Viktor Ivanov <sessgreem@gmail.com>
2018-07-15 19:13:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+function getIt(){
+  return $('p').on('click',function(){
+    window.alert('Hey!')
+    return;
+  })
+}
+
+function frameIt(){
+  return $('img').on('load',function(){
+    $('img').addClass('tasty');
+  return;
+  })
+}
+
+function pressIt(){
+  return $('input').on('keydown',function(e){
+    if(e.which === 71){
+      window.alert("You've pressed G key!")
+      return;
+    }
+  })
+}
+
+function submitIt(){
+  return $('body').on('submit',function(){
+    window.alert('Your form is going to be submitted now.')
+    return;
+  })
+}
+
 
 $(document).ready(function(){
-
-// call functions here
-
+getIt()   
+pressIt() 
+submitIt() 
+frameIt()
 });
