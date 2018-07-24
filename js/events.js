ffdbd73ce801ae2f837e9558a21de48ff1fc1cef
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-24 02:26:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
-
 });
+
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('G was pressed')
+    }
+  })
+}
+ 
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
