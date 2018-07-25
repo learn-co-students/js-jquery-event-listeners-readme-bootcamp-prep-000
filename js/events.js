aa623c1b00b899991df27028bed2c9015b0af519
Richard MacCaw <ricci6@gmail.com>
2018-07-25 11:27:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,29 @@
 
 $(document).ready(function(){
 
+getIt()
+frameIt()
+pressIt()
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function () {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt () {
+  $('#typing').on('keydown', function (e) {
+    if (e.which === '71') {
+      alert('You pressed G')
+    }
+  })
+}
