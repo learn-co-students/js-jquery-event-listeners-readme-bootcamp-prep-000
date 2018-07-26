2b7fadedaceeb3a003f64ec2e9c637ad83a0f7d6
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-26 22:45:20 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,48 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt(){
+  $(`p`).on(`click`, function(){
+    alert("Hey!")
+  })
+}
+
+
+function frameIt(){
+  
+  $(`img`).on(`load`,function(){
+    $(`img`).addClass(`tasty`)
+  })
+  
+}
+
+function pressIt(){
+  
+  $(document).on(`keydown`,function(key){
+    if(key.which === 71){
+      alert("G key was pressed")
+    }
+  })
+}
+
+function submitIt(){
+  
+  $("form").on("submit",function(){
+    alert("Your form is going to be submitted now.")
+   
+  })
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
