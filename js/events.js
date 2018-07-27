f7f0b6ae48bea05cef5f4bd1208d36af0c3592f3
karen S <buzz.kyt@gmail.com>
2018-07-27 23:06:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+  alert('Hey!');
+});
+}
+  
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+});
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+  alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt(){
+  $('#typing').on("keypress", function(event) {
+  if (event.which == 71) {
+  alert("G!");
+    }
+});
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+ 
+ getIt();
+ frameIt();
+ pressIt();
+ submitIt();
+ 
 });
