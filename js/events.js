33b59c3e1376570881848740c600a0db73f2fd3e
Brian Zhang <brianzhangw@gmail.com>
2018-07-30 03:04:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,36 @@
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
+
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $(this).addClass("tasty");
+    $(this).css('border', "solid 2px red");  
+  });
+}
+
+function pressIt(){
+  $('input').on("keydown", function(key){
+    if(key.which == 71){
+      alert("G pressed");
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
