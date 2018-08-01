51e5f7ca935690daa92c8d06defddaab7b38cf8d
Juna Luzi <junaluzi@gmail.com>
2018-08-01 03:51:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
 
 $(document).ready(function(){
-
+  $('p').on('click', getIt);
+  $('input').on('submit', submitIt)
 // call functions here
 
-});
+})
+
+function getIt(){
+    alert('Hey!');
+}
+
+function frameIt(){
+  $('img').attr('class', 'tasty');
+  $('.tasty').css('border', 'red');
+}
+
+function pressIt(){
+  $('input#typing').on('keydown', function(key){
+    if(key.which == 71){
+      alert('You have pressed letter G.')
+    }
+  })
+}
+
+function submitIt(){
+    alert("Your form is going to be submitted now.")
+}
