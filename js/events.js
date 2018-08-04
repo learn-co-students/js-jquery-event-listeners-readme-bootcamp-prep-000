f4c71599d825ce90ce68a1be94361f2a9ce0b6a0
Mai Campbell <jirallc@gmail.com>
2018-08-04 16:36:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    window.alert('Hey!')
+  });
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert("You've presses the G key");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    window.alert('Your form is going to be submitted now.')
+  })
+}
 $(document).ready(function(){
+  getit();
+  frameIt();
+  pressIt();
+  submitIt();
 
 // call functions here
 
