82d45c48721692725e5dfcd0e01faf376d604d57
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 20:20:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+function getIt(){
+  $('p').click(function(){
+    alert('Hey!');
+  })
+}
+
+function frameIt(){
+  $('img').load(function(){
+    this.className = 'tasty';
+  })
+}
+
+function pressIt(){
+  $('input').keydown(function(e){
+    if (e.which === 71){
+      alert("You pressed G.")
+    }
+  })
+}
+
+function submitIt(){
+  $('form').submit(function(e){
+    alert("Your form is going to be submitted now.");
+  })
+}
+
