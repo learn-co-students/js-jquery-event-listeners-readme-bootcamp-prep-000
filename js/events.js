29fa7d2fe275c9864d2091a3173c72bb85085bfa
Malik Glossop <malik.glossop@gmail.com>
2018-08-12 23:54:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert("Hey!")
+  })
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(e) {
+    if(e.which === 71) {
+      alert("G was pressed")
+      return
+    }
+  })
+}
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+    return
+  })
+}
 $(document).ready(function(){
-
-// call functions here
+getIT()
+frameIt()
+pressIt()
+submitIt()
 
 });
