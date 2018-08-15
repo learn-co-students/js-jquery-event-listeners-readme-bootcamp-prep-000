d9e7422104773f363004e1db76111b58886f30b2
Casey Zimmer <czimmer@ramapo.edu>
2018-08-15 02:59:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
+  
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+
+function getIt()
+{
+  $('p').on("click",function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt()
+{
+  $("img").on("load",function(){
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt()
+{
+  $("form").on("keydown",function(key){
+    if(key.which == 71){
+      alert("G has been pressed");
+    }
+  })
+}
+
+function submitIt()
+{
+  $("form").on("submit",function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
