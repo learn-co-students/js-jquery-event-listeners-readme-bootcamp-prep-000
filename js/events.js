92cd0f8423447b25b3cfce34d444381b4561ce2c
ahmed atta <ahmedatta3322@gmail.com>
2018-08-22 19:40:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
-
+function getIt(){
+  return $('p').on('click' , window.alert('hey'))
+}
+function frameIt(){
+  $('img').on('load' , function(){
+    $('img').addClass("tasty")
+  })
+} 
+function pressIt(){
+  $('#typing').on('keydown' , function(e){
+    e = e.which
+    if (e === 71){
+      window.alert("You pressed 'g'!")
+    }
+  })
+}
+function submitIt(){
+  $('form').on('submit',function(){
+    window.alert('Your form is going to be submitted now.')
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+pressIt()
+getIt()
+frameIt()
+submitIt()
 });
