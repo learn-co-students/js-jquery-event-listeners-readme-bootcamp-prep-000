9351729b47417f72b9262b9dcce78c4ee8266e7f
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-24 22:55:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,18 @@
 //define functions here
 
+function getIt() {
+  alert("Hey!")
+}
+
+function frameIt() {
+  $("img").addClass("tasty");
+}
+
+
 $(document).ready(function(){
 
+$('p').on("click", getIt());
+on("load", frameIt())
 // call functions here
 
 });
