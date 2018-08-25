e2d703853513ac4a447bdf887bff6ea770d7f34d
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-25 00:25:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,13 +6,29 @@ function getIt() {
 
 function frameIt() {
   $("img").addClass("tasty");
+}  
+
+function pressIt() {
+  $('form#typing').on("keydown", function(e) {
+    if (e.which === 71) {
+      window.alert("the key is G")
+    }
+  })
+}
+
+  
+function submitIt() {
+  alert("Your form is going to be submitted now.")
 }
 
 
 $(document).ready(function(){
 
 $('p').on("click", getIt());
-on("load", frameIt())
+on('load', frameIt());
+$('form#typing').on("keydown", pressIt());
+$('form').on("submit", submitIt());
+
 // call functions here
 
 });
