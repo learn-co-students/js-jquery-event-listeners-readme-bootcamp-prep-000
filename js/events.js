beaf4553521eb524fde72d4c89b3724f31ae8b1d
Sam B <s77broz@aol.com>
2018-09-01 04:28:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
+function getIt() {
+  $("p").on("click", () => {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", () => {
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $(document).on("keydown", e => {
+    if (e.which === 71) {
+      alert("You pressed G!");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", () => {
+    alert("Your form is going to be submitted now.");
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
 });
