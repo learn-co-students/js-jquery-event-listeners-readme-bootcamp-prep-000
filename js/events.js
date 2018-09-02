3bb0f54d4600f98c71d6cbdaaa158ea9781ac316
jill zhang <talktojillzhang@gmail.com>
2018-09-02 06:41:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
 $(document).ready(function(){
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 // call functions here
 
 });
+function getIt(){
+  $('p').on("click",function(){
+    alert("Hey!")
+  })
+}
+function frameIt(){
+  $('img').on("load",function(){
+    $('img').addClass("tasty")
+  })
+}
+function pressIt(){
+  $('input').on("keydown",function(e){
+    if (e.key === 'G'){
+      alert("blah blah bal")
+    }
+  })
+}
+function submitIt(){
+  $('form').on("submit",function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
