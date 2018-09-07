4f4673b51471c6ea12cfe137f78333ec78c0a7c6
Annie Eby <eby.annie@gmail.com>
2018-09-07 14:29:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,14 +6,58 @@ $("p").on("click", function() {
   });
 }
 
+function frameIt() {
+$('img').on("load", function() {
+  $('img').addClass("tasty")
+  });
+}
+
+function pressIt() {
+$('input').on('keydown', function(key) {
+  if (key.which == 71){
+    alert('g was pressed');
+  }
+});
+}
+
+function submitIt() {
+$("form").on("submit", function() {
+  alert ("Your form is going to be submitted now.")
+})
+}
+
+
+
 $(document).ready(function(){
 
 getIt() 
 
+frameIt()
+
+pressIt()
+
+submitIt()
+
 })
 
 /*
-OLD DRAFTS
+ALT ATTEMPTS
+
+function frameIt() {
+$("img'").on("load", function() {
+  $("img").addClass("tasty")
+  });
+}
+
+function frameIt() {
+var images = document.getElementsByTagName("img");
+var i;
+
+for(i = 0; i < images.length; i++) {
+    images[i].className += "tasty"
+    }
+}
+
 function getIt() {
 $("p").on("click", function() {console.log("Hey!")});
 }
@@ -40,8 +84,8 @@ function getIt() {
   $('p.alert').click(function(){
     alert("Hey!");
 })
-*/
 
 //Difference between .on('click') vs .click() - Stack Overflow https://stackoverflow.com/questions/9122078/difference-between-onclick-vs-click
 
 });
+*/
