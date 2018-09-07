5fb58096659692455fd852fcce8b99f0a31d646c
Annie Eby <eby.annie@gmail.com>
2018-09-07 13:30:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,47 @@
 //define functions here
 
+function getIt() {
+$("p").on("click", function() {
+  alert ("Hey!")
+  });
+}
+
 $(document).ready(function(){
 
-// call functions here
+getIt() 
+
+})
+
+/*
+OLD DRAFTS
+function getIt() {
+$("p").on("click", function() {console.log("Hey!")});
+}
+
+
+function getIt() {
+$("p").on("click", function() {alert ("Hey!")});
+return;
+}
+
+$(document).ready(function(){
+
+getIt() 
+
+  
+})
+
+function getIt() {
+  $('p').on("click", 'button.alert', function() {
+    alert("Hey!");
+}) 
+
+function getIt() {
+  $('p.alert').click(function(){
+    alert("Hey!");
+})
+*/
+
+//Difference between .on('click') vs .click() - Stack Overflow https://stackoverflow.com/questions/9122078/difference-between-onclick-vs-click
 
 });
