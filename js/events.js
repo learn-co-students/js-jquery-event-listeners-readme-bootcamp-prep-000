64d0ed121bf52f46ec9407926d897c90cc6df766
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-08 21:04:07 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+function getIt(){
+  $("p").on('click', function (e){
+    alert ("Hey!");
+    return;
+  });
+}
+
+function frameIt(){
+  $("img").on("load", function(e){
+    $("img").addClass('tasty');
+    $('img')[0].style.border = '3px solid red';
+  });
+}
+
+function pressIt(){
+  $('input').on("keydown", function (e){
+    if (e.which === 71){
+      alert("You pressed G");
+      return;
+    }
+  });
+}
+
+function submitIt(){
+  $("form").submit(function(event){
+    alert("Your form is going to be submitted now.");
+    event.preventDefault();
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
