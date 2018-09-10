1711865dd1c92ef6819d8003d99dff7c87c6c02e
demian sims <demiansims@gmail.com>
2018-09-10 16:02:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -13,7 +13,26 @@ function getIt() {
 function frameIt() {
   $('img').on('load', function() {
    
-   $('img').append()  
-})
+   $('img').addClass('tasty')
+});
 }
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('G was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on('submit', function() {
+  
+      alert("Your form is going to be submitted now.");
+      return;
+    
+  });
+}
+  
+
 
