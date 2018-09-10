986f54cea31591b7c06cfd988fee8f8d821420bc
Joon Jang <joon.jang95@gmail.com>
2018-09-10 17:41:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert('hey!');
+    return;
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function(e) {
+    $('img').addClass('tasty')
+    return;
+  })
+}
 
-// call functions here
+function pressIt() {
+  $('form').on('keydown', function(e) {
+    if (e.which === 71) {
+           alert('G was pressed')
+           return;
+    }
+  })
+}
 
-});
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
