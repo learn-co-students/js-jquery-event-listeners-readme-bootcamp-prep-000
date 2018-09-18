48e0db1428804b340bf959ed5e8e890d09dfb2ad
Matthew Migliore <mmigliore311@gmail.com>
2018-09-18 12:17:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+    return
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('You Pressed "g"')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
