dc9fb449d0ac1a4492c081605288da018ec3b3d3
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-22 19:56:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt(){
+$('p').on('click', alert('Hey!'))
+}
+function frameIt(){
+$(document).on('load', function(){
+  $("img").addClass("tasty")
+})
+}
+function pressIt(){
+$('input').on('keydown', function(key){
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+})
+}
+function submitIt(){
+$('form').on('submit', alert('Your form is going to be submitted now.'))
+}
+
 
 $(document).ready(function(){
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 // call functions here
 
 });
