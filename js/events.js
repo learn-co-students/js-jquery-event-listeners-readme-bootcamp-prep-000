dd9e684eb7bab5b86e8ba8ae0d973d7043e0e3d4
Dan Brimhall <dpbrimhall@hotmail.com>
2018-09-23 19:14:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  var addClass = document.querySelector('img');
+  addClass.className = "tasty"
+  return
+}
+
+function pressIt() {
+   $('input').on('keydown', function(e) {
+     if (e.which == 71) {
+     alert('You have pressed the "G" key')
+     }
+     else return
+   }) 
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt(); 
+  frameIt();
+  pressIt();
+  submitIt();
+  return
 });
