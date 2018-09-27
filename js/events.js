c6b791adf8470eadb034f722374951c1834a12cc
Jackson Bates <jkbates222@gmail.com>
2018-09-27 20:51:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,35 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+submitIt();
+pressIt();
 
 });
+
+function getIt() {
+  $("p").on('click', function() {
+    alert("Hey!");
+  });
+}
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass("tasty");
+    $(this).css('border', "solid 2px red");
+  });
+}
+function submitIt() {
+  if (alert("Your form is going to be submitted now.")) {
+    $("form").on("submit", function() {
+    });
+  }
+}
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("G was pressed");
+    }
+  });
+  
+}
