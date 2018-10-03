5f1a7cca704afb4298d30ff30f92c10a2f332336
Johnathon Wood <jaywood128@gmail.com>
2018-10-03 16:46:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,16 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+ })
+}
+function frameIt() {
+  $('img').on ("load", function() {
+    $('img').addClass('tasty')
 
+  })
+}//define functions here
 $(document).ready(function(){
 
-// call functions here
-
+ //getIt();// call functions here
+  frameIt();
 });
