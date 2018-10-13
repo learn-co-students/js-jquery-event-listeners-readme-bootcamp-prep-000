f5b387603235a9394b9910174525ae64dcfe8498
Kara Isreal <isreal.kara92@gmail.com>
2018-10-13 21:22:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(key){
+  alert("Hey!")
+});
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+  $(this).addClass('tasty')
+});
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if (key.which == 71){
+      alert();
+    }
+  });
+}
+
+function submitIt(){
+$("form").on("submit", function(form) {
+    alert('Your form is going to be submitted now.');
+});
+}
 
 $(document).ready(function(){
 
