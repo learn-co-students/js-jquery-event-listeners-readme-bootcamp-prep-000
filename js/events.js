b441a746a35946d53d5e1c86c2ff64668d11f47f
gc6123 <gc6123@bard.edu>
2018-10-23 19:30:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
-//define functions here
-
+function getIt() {
+  $('p').on('click');
+  alert("Hey!");
+}
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+function pressIt() {
+  $('input').on('keydown', function(event) {
+    if (event.which === 71) {
+      alert('G was pressed');
+  }});
+}
+function submitIt() {
+  $('submit').on;
+  alert("Your form is going to be submitted now.");
+}
 $(document).ready(function(){
 
-// call functions here
+submitIt();
 
 });
