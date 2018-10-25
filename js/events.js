143c25d93e6bd207ba73f34a32b39fc90aac6f2f
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-25 04:49:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
-
 $(document).ready(function(){
+ // call functions here
+ getIt();
+ frameIt();
+ pressIt();
+ submitIt();
 
-// call functions here
+ });
 
+
+function getIt() {
+  $('p').on('click')
+  return alert('I was clicked!')
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(key) {
+    if(key.which === 71) {
+    alert('ooooo u did it now my G');
+  }
 });
+}
