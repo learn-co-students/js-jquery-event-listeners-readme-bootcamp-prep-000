063d4abc1e18ece645d51ba348e63ba42c5eee3a
Jarrod Dyer <jarrod@gasganno.com>
2018-10-30 23:40:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,92 @@
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 //define functions here
 
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $("img").addClass( "tasty");
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71) {
+    alert('G was pressed');
+  }
+})
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+$(document).ready(function(){
+
+getIt()
+frameIt()
+pressIt()
+submitIt()
+
+});
+*/
