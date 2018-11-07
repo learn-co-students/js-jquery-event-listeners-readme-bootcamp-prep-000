cf2ba30b95d8a2d30aa85e6d6ddda23736e5ad41
Brian <briandsalemi@gmail.com>
2018-11-07 16:15:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
-//define functions here
 
-$(document).ready(function(){
+function getIt() {
+  const para = document.querySelector('p');
+  para.addEventListener('click', function(eventname) {
+  alert("Hey!");
+})}
 
-// call functions here
 
+$(document).ready(function getIt() {
+});
+
+
+function frameIt() {
+  const tasty = document.querySelector('img');
+  tasty.classList.add("tasty");
+  tasty.addEventListener('load', function(eventname) {
+  }
+  )}
+
+
+$(document).ready(function frameIt() {
 });
