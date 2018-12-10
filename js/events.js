3aab96c44a835281e41c461bc35c852ce67794e7
Matthew Campbell <matjocampbell@gmail.com>
2018-12-10 00:14:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+//works
+function getIt(){
+  $("#only-text").on("click", function(){
+    alert("Hey!");
+  });
+}
+
+//works
+function frameIt() {
+  $("#only-pic").on("load", function(){
+    $("#only-pic").addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown", function(event){
+    const key = event.key;
+    
+    if (key === "G") {
+      alert("IT'S ALIVE!");
+    } 
+  });
+}
+
+//this function fails
+function submitIt() {
+  $("form:nth-child(2)").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
