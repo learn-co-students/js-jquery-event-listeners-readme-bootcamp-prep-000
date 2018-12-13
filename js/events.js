2b06fce8eb8bc29c01b6c0dfa7622fe325bab5ca
Naenae L <oneprincesslea@gmail.com>
2018-12-13 17:49:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+$('p').on('click', 
+  function getIt(){
+    alert("Hey!"); 
+  }); 
+  
+$('img').on('load', 
+  function frameIt(){
+    $('img').addClass("tasty"); 
+    imgTag.style.border='red'; 
+  }); 
+  
+  $('form').on('keydown', 
+  function pressIt(){
+    if (key.which==71){
+      alert('You pressed G! OMG!'); 
+    } 
+  }); 
+  
+$('form').on('submit', 
+  function submitIt(){
+      alert('Your form is going to be submitted now.'); 
+  }); 
 
 $(document).ready(function(){
 
 // call functions here
-
+function getIt(); 
+function frameIt();
+function pressIt(); 
+$('form').on('submit', 
+  function submitIt(){
+      alert('Your form is going to be submitted now.'); 
+  }); 
 });
