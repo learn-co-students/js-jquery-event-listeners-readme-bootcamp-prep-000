6978ec928e1cb553cab70b3394c78785dde47f8c
Maximillian Schulte <maximilliangschulte@gmail.com>
2018-12-19 06:17:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
+function getIt(){
+  $('p:first').on('click', function(){
+    alert('Hey!')
+  })
+}
+function frameIt(){
+  $('img:first').on('load', function(){
+    $('img').addClass("tasty")
+  })
+}
+function pressIt(){
+  $('form #typing').on('keydown',function(key){
+    if(key.which ===71){
+      alert("You have pressed the 'G' key")
+    }
+  })
+}
+function submitIt(){
+   $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
