24af6a9dee633ca24d32f86c8880b5fc4cc67c51
Travis Martin <travisemerymartin@gmail.com>
2018-12-28 00:02:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,12 +1,36 @@
 //define functions here
 function getIt() {
-  $('p').on('click', => {
-    alert("Hey!")
+  $('p').on('click', event => {
+    alert("Hey!");
+    return
   });
 }
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', event => {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $(document).on("keydown", event => {
+    if (event.key === "g") {
+      alert('You have pressed the G key');
+    };
+  });
+}
+
+function submitIt() {
+  $(document).on("submit", event => {
+    alert('Your form is going to be submitted now.')
+  });
+}
+
+$(document).ready(function() {
 getIt();
+frameIt();
+pressIt();
+submitIt()
 // call functions here
 
 });
