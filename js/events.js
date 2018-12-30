87ab7f166e5c074434811096f21cca86694c6456
Travis Martin <travisemerymartin@gmail.com>
2018-12-30 17:22:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('body form').on('keyup', event => {
+  $('body form').on('keydown', event => {
     if (event.key === 'G') {
       alert('You have pressed the G key')
     };
