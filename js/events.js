581de072c759e25b03bfbd0eabcdd3a2915090bb
David Stewart <davidstewart@gmail.com>
2019-01-22 20:30:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -4,4 +4,37 @@ $(document).ready(function(){
 
 // call functions here
 
-});
+})
+function getIt() {
+  $('p').on("click",function() {
+    alert('Hey!');
+  })
+}
+
+
+$('img').on('load',frameIt)
+
+function frameIt () {
+    $('img').addClass('tasty')
+    $('img').css('border', "solid 2px black")
+  }
+
+ $('#typing').on('keydown', pressIt)
+ 
+function pressIt() {    
+        $('#typing').on('keydown', function(key) {
+  if (key.which === 71){
+    alert('enter was pressed');
+  }
+});  
+    }
+    
+
+
+function  submitIt() {
+        $('form').on('submit',function() {
+    alert('Your form is going to be submitted now.');
+  }
+)}
+
+
