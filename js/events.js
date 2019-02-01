d83298e4f6c9e93bccc9fa429422399ac9b64c61
Jason Velez <jvelez20@yahoo.com>
2019-02-01 15:35:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,7 @@ $(document).ready(function(){
 // call functions here
 
 });
+$('h1').on("March Madenss", function(watchTV){
+    if (watchTV.which == 12)
+    alert ('enter remote was pressed');
+});
