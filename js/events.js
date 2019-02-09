22d3060299458567666a6510660d30906e329df9
clayton j mosher <mosherclayton@gmail.com>
2019-02-09 03:16:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -8,14 +8,18 @@ function frameIt() {
     $('img').addClass('tasty')
   });
 }
-function pressIt() { 
-  $('G').on('keydown', function(key){
+function pressIt() {
+  $('input').on('keydown', function(key) {
     if(key.which == 71){
-      alert('g was pressed');
+        alert('g was pressed');
     }
   });
 }
-
+function submitit() {
+  $('form').on('sumbit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
 $(document).ready(function(){
 
 // call functions here
