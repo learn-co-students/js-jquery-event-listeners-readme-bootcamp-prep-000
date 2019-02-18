19a06874c8e65f0695929571f9109339003fdebd
Mike Doig <mike.j.doig@gmail.com>
2019-02-18 20:10:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,25 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function (){
+    alert('Hey!');
+  });
+}
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+function submitIt() {
+  $('form').on('submit',function() {
+  alert ('Your form is going to be submitted now.');
+});
+}
+function pressIt() {
+ $('form').on('keydown', function (e){
+   if (e.which == 71) {
+   alert('You pressed the G key!');
+}
+});
+}
 
 $(document).ready(function(){
 
