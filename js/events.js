2f82830118874ef50765e1a41a189b943dfa04b4
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-15 16:17:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,45 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function()
+  {
+    return alert("Hey!");
+  });  // have a click event on <p> and alert("Hey!")
+}
+
+function frameIt()
+{
+   $('img').on('load', function()
+   {
+     // add a red frame here
+     $('img').addClass('tasty');
+   });
+}
+
+function pressIt()
+{
+  $('input').on('keydown', function(key){
+      if(key.which == 71)
+      {
+        return alert("G has been pressed");
+      }
+  });
+}
+// event.keydown = g
+
+function submitIt()
+{
+  $('form').on('submit' , function()  // tried going for the 'input' directly
+  {
+    return alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
