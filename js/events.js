333968fd40c47ac2b87f31901ddd957e36972c09
Erini George <dr.erinigeorge@yahoo.com>
2019-04-08 13:07:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,43 @@
 //define functions here
 
+function getIt()
+{
+  $('p').on('click', alert('Hey!'));
+}
+
+function frameIt()
+{
+  $('img').on('load', function() {
+    $('img')[0].className = 'tasty';
+      })
+}
+
+function pressIt()
+{
+  $("#typing").on('keydown', function(e){
+    if (e.which == 71)
+    {
+      alert('G');
+    }
+    
+  })
+}
+
+function submitIt()
+{
+  $('form').on('submit', alert("Your form is going to be submitted now."));
+}
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt();
+
+frameIt();
+
+pressIt();
+
+submitIt();
 
 });
