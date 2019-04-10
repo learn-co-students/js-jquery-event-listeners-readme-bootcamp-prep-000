6b5fd56f6177bd646844f0b3f32cdb0892e6b3e0
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-10 22:44:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,9 +1,18 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
 
+function frameIt() {
+ return $('img').on('load', function() {
+   $('img').addClass('tasty');
+ }); 
+}
 
 
 $(document).ready(function(){
-
 // call functions here
 alert(frameIt);
 });
