89ad8973fb2f24e8124f88998cf9f3d6bf7f0bcd
Beto Dute <betodute@gmail.com>
2019-05-13 09:16:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+  function getIt(){
+    $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+  function frameIt(){
+    $("img").on("load", function(){
+      $("img").addClass("tasty");
+    });
+  }
+  
+  function pressIt(){
+    $("input").on("keydown", function(g){
+      if(g.which === 71){
+        window.alert("Hey homie you pressed G");
+      }
+    });
+  }
+  
+  function submitIt(){
+    $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+    });
+  }
 
 $(document).ready(function(){
-
-// call functions here
 
 });
