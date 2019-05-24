c012aeb42b39e673b73551d1fa3642d81ec276ed
thespecialk1 <thespecialk1@gmail.com>
2019-05-24 16:53:20 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,35 @@
 //define functions here
 
+function getIt () {
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt () {
+  $(document).ready(function(){
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt () {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('s was pressed');
+  }
+});
+}
+
+function submitIt () {
+  $("form").on("submit", function() {
+
+  alert("Your form is going to be submitted now.");
+
+});
+}
+
+
+
 $(document).ready(function(){
 
 // call functions here
