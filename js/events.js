69763fe57cd9c3f73101a18f18e81df5355af14c
communal-principle-3181 <aellis19@outlook.com>
2019-05-27 23:16:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,39 @@
 //define functions here
+function getIt()
+{
+  $('p').on('click', function()
+  {
+    alert('Hey!')
+    
+  });
+}
+
+function frameIt()
+{
+  $('img').on('load',function()
+  {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt()
+{
+  $('input').on('keydown', function(e)
+  { 
+    if (e.which === 71)
+    {
+      alert('G key pressed');
+    }
+  });
+}
+
+function submitIt()
+{
+  $('form').on('submit', function()
+  {
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
