5ef1cbee4f64f551868ed2f6608972d3750150c2
David Strauch <dmaster18@gmail.com>
2019-06-18 16:05:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(key){
+    if(key){
+    alert("Hey!");
+    return;
+    }
+    })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+      $('img').addClass('tasty');
+    })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key) {
+    if(key.which === 71){
+      alert('G was pressed');
+      return;
+    }
+  })
+}
+
+function submitIt() {$('form').on('submit', function(){
+  alert('Your form is going to be submitted now.');
+})
+}
 
 $(document).ready(function(){
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 // call functions here
 
 });
