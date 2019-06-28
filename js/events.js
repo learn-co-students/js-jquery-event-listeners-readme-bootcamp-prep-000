7664f7a81ad394fbae241916bec59a736f61b589
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-28 04:10:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
 
+
+
+function getIt(){
+  var pTag = $("p");
+  pTag.on("click",function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  var image = $("img");
+  image.on("load",function(){
+    image.addClass("tasty");
+  });
+}
+
+function pressIt(){
+  var input = $("input");
+  input.on("keydown",function(e){
+    if(e.which ===71){
+      alert("G key pressed");
+    }
+  });
+}
+
+function submitIt(){
+  var form = $("form");
+  form.on("submit",function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
