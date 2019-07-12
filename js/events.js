83a17d1ca71839c9e56181ef8eb8017712be0928
Aude Faucheux <aude53@gmail.com>
2019-07-12 20:33:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    return alert('Hey!')
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function() {
+    return $('img').addClass('tasty')
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which === 71) {
+      return alert('You pressed g');
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    return alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
