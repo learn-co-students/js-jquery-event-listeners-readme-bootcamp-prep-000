c42c0156310379d650ec741d56f91c9989a31c5c
Brandon Fries <brandonfries1993@gmail.com>
2019-07-16 17:17:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("hey!")
+  })
+}
+
+function frameIt(){
+  $("img").on('load', function() {
+    $(this).css('border', "solid 2px red").addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $("input#typing").on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("g was pressed")
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+submitIt()
+getIt()
+frameIt()
+submitIt()
 
 });
