34fc5979b058ecda81e67b2388ab78bcbbff6ee2
Aaron Blum <blumaa@gmail.com>
2019-07-22 22:12:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,29 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').bind('click', function() {
+    alert('Hey!');
+  }
+);
+
+}
+
+function frameIt() {
+  $('img').addClass("tasty");
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which == 71){
+        alert('s was pressed');
+   }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+})
+}
