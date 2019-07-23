cba6e0d9de97244c5a986a8f6f81faf4ca1592ef
Chris Lyons <chris@lyonsgroup.family>
2019-07-23 00:13:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $("form").on("keydown", function(e) {
+    if (e.which == 71) {
+      alert("pressed g");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    alert("Your form is going to be submitted now.");
+    return ;
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
