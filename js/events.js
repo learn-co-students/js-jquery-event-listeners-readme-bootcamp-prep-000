5a50002b75c95043047e741f2f731926e08d9f07
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-27 19:11:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,34 @@
 //define functions here
+function getIt(){
+  $("p").click(function(){
+    alert ("Hey!")
+    return;
+  })
+}
+function frameIt(){
+  $("img").on('load',function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if (key.which==71){
+      alert ("G was pressed")
+      return
+    }
+  })
+}
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+    return
+  })
+}
+
+
+
+
+
 
 $(document).ready(function(){
 
