451d7f377cc5712fb6895d432ffdb19c906b5262
Georgi Petrushev <figoto660@gmail.com>
2019-10-06 17:02:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,45 @@
 //define functions here
+function getIt(){
+  $("p").on("click",function(){
+    alert("Hey!");
+  })
+  }
+
+function frameIt(){
+  $("img").on("load",function(){
+    $("img").addClass("tasty");
+    
+  })
+}
+
+function pressIt(){
+  $(document).on("keydown",function(key){
+    if(key.which === 71){
+      alert("You have press 'G'!")
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit",function(){
+    alert("Your form is going to be submitted now.");
+    consol.log("test");
+  })
+}
+
+
 
 $(document).ready(function(){
+    getIt();
+    frameIt();
+    pressIt();
+    sibmitIt();
 
-// call functions here
-
+    
+  
+  
+  
 });
+
+      
+        
