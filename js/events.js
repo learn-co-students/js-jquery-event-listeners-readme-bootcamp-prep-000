d5e4e665fb0da73088666e3c08542328fc211582
Gabriel Onike <onikegabriel@gmail.com>
2019-10-28 17:22:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,11 +3,33 @@ function getIt(){
   $("p").on('click', function(p){
     alert('Hey');
   });
+};
+
+function frameIt(){
+  $('img').on('load', function(img){
+    $("img").addClass("tasty");
+  });
+};
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if (key.which==71) {
+      alert();
+    }
+  });
 }
 
+function submitIt(){
+  $('form').on('submit',function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-   $("p")
+   $("p");
+   $("img");
+   $("key");
+   $("submit");
 // call functions here
 
 });
