8369c79cf172d3c1fedd5d85ca49cb55789e343d
Gabriel Onike <onikegabriel@gmail.com>
2019-10-28 16:45:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
+function getIt(){
+  $("p").on('click', function(p){
+    alert('Hey');
+  });
+}
+
 
 $(document).ready(function(){
-
+   $("p")
 // call functions here
 
 });
