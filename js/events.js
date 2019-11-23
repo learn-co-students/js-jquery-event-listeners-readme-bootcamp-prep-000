709045534c2b7f38ecbe673b5049de500524a332
Liam Argent <liargent@hotmail.com>
2019-11-23 20:01:06 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!"); 
+  }); 
+}
+
+function frameIt() {
+  $("img").load(function() {
+    $(this).addClass("tasty"); 
+  }); 
+}
+
+function pressIt() {
+  $("#typing").on("keydown", function(e) {
+    if(e.which == 71) {
+      alert("You pressed the G key, congratulations."); 
+    }
+  }); 
+}
+
+function submitIt() {
+  $("form").submit(function(event) {
+    alert("Your form is going to be submitted now."); 
+  }); 
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt(); 
+  
 });
