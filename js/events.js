0905bd80b27bd2c59df7afe20ce21074a53ac874
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-10 13:03:04 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,18 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+      //action you want taken
+      alert('Hey!');
+  });
+};
 
-$(document).ready(function(){
+function frameIt() {
+  $('.tasty').on("load", function (){
+    return $('.tasty');
+  });
+};
 
+$(document).ready(function() {
 // call functions here
-
+getIt();
 });
