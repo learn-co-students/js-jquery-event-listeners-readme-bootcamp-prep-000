488acf4904e9c7d8da1c35bc684a484a7d7e8be6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-10 13:31:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -7,12 +7,31 @@ function getIt() {
 };
 
 function frameIt() {
-  $('.tasty').on("load", function (){
-    return $('.tasty');
+    console.log("test");
+  $('img').on('load', function (){
+    $('img').addClass('tasty');
+
+  });
+};
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+};
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
   });
 };
 
 $(document).ready(function() {
 // call functions here
 getIt();
+frameIt();
+pressIt();
+submitIt();
 });
