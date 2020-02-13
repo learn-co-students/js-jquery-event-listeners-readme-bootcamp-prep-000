801a1af2f1e0bcd584877237104c8f96e46dc697
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-13 15:57:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,31 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', () => {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', () => {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', (key) => {
+    if(key.which == 71) {
+      alert("the letter g has been pressed, commence nuclear meltdown");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
