00a57da351abf4900dbf03e2d3105e351ad0f00b
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-18 08:36:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt()
+{
+  $('p').on("click", function(){
+    //action you want taken
+    alert("Hey!");
+});
+}
+function frameIt()
+{
+    $("img").addClass('tasty').on("load", function(){
+   
+   });
+}
+function pressIt()
+{
+  $("form").on("keydown", function() {
+    if ($( "input:first" ).val() === "G") {
+    alert('G is pressed');
+    return;
+  }
+})
+}
+function submitIt()
+{
+  $("form").on("keydown", function() {
+    
+    alert("Your form is going to be submitted now.");
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
