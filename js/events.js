b2436ab847793dab095a089d9dc41a87efd4cbe3
Jeff Simon <jeffsbillinfo@gmail.com>
2020-02-23 03:38:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,51 @@
 //define functions here
 
+function getIt()
+{
+  $('p').on("click", function() {alert("Hey")});
+}
+
+function frameIt()
+{
+  let chrome_photo = window.$('img');
+  var img = new Image();
+    img.addEventListener('load', 
+    function ()
+    {
+    chrome_photo[0].setAttribute("class", "tasty");
+    },
+    true);
+  img.src = "https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg";
+  
+}
+
+function pressIt()
+{
+  $('form #typing').on('keydown',
+  function(e)
+    {
+      if(e.key === 'G')
+      {
+        alert('You have pressed the "G" key');
+      }
+    }
+  );
+}
+
+function submitIt()
+{
+  var test = $(':submit');
+  console.log(test);
+  console.log("hello");
+}
+
+
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
