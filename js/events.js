d521361fbc50bc7544fd854325dddf3538339941
Dave Patel <ddp2134@gmail.com>
2020-02-25 00:36:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,27 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+  
+function pressIt(){
+  $("input").on("keydown", function(e){
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your forim is going to be submitted now.");
+  })
+}
