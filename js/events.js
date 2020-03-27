617d73e3e03607c5f4152681c9a07bc2d1add894
Steven Luck <steve.luck@gmail.com>
2020-03-27 19:53:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+  alert("Hey!");
+}
 
+function frameIt() {
+  $('img').addClass("tasty");
+}
+
+function pressIt() {
+  $('input').on("keydown", function(key) {
+    if(key.which == 71){
+      alert('hey! You pressed g!')
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+   {
+    $('p').on("click", getIt());
+    $('img').on("load", frameIt());
+    
+  }
 });
+
