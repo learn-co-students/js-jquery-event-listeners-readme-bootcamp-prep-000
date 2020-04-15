5ace52d0fba8f6ba56f184bf0787b392ffa1d5ec
Jake G <jake098877@gmail.com>
2020-04-15 15:18:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+function getIt() {
+  alert("Hey!")
+}
+
+function frameIt() {
+ $("img").addClass("tasty");
+  'img'.class ='tasty'
+}
+
+function pressIt() {
+   if ($('input') == 'G') {
+    alert('You pressed a G key')
+    return
+  }
+  else {
+    return
+  }
+
+
+}
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+  return
+}
 
 $(document).ready(function(){
 
-// call functions here
+  $('p').on('click',getIt())
+
+  $('img').on('load', frameIt())
+
+  $("input").on('keydown', pressIt())
+
+
+  $("form").on("submit", submitIt())
 
 });
