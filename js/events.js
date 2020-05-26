541a2e82f36e48e62498814cdd0f8f4d5ab268c5
Michelle Lee <mlee092@Michelles-MacBook.local>
2020-05-26 10:42:44 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
-$(document).ready(function(){
-
-// call functions here
-
-});
+function getIt(){
+    return $('p').on('click', alert('Hey!'));
+  }
+  
+  function frameIt(){
+    return $('img').on('load', function (){
+       $('img').addClass('tasty');
+    });
+  }
+  
+  function pressIt(){
+    return $('#typing').on('keydown', function (l){
+      if(l.which == 71){
+        alert('G was pressed');
+      }
+    })
+  }
+  
+  function submitIt(){
+    return $('form input:last').on('submit', alert("Your form is going to be submitted now."));
+  }
+  
+  $(document).ready(function(){
+  
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+  });
