de9a2b6b013ff572124045555fc6aaa2bc24537e
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 15:41:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $(window).on('load', function(e) {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('You pressed G')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
