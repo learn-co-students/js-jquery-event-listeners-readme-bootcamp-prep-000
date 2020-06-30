537c43f9e72e059a786c445075158478b0d7c912
Daniel Gould <dmgould310@gmail.com>
2020-06-30 18:24:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click', function (){
+   window.alert('Hey!'); 
+  });
+}
 $(document).ready(function(){
 
 // call functions here
 
 });
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if(key.which === 71){
+      alert('G was pressed');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    window.alert('Your form is going to be submitted now.');
+  })
+}
+
+frameIt();
+pressIt();
+submitIt();
+  
