df00f1c0791db1cbe5e61aa9db6a302556a787eb
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-18 03:53:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,33 @@
 //define functions here
+function getIt(){
+  $('p').on('click',function(){
+    alert("Hey!")
+  })
+  return;
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    this.className = 'tasty'
+  })
+  return;
+}
+
+function pressIt(){
+  $('input').on('keydown',function(key){
+    if(key.which === 71){
+      alert("You pressed down the g key");
+    }
+  })
+  return;
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  })
+  return;
+}
 
 $(document).ready(function(){
 
