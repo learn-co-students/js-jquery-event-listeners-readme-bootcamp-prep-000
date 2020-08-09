52dc32f30c60d22a3f6db83ceb39dac43df26615
Briana Oniru <damioniru16@gmail.com>
2020-08-09 22:22:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,31 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+  
+function frameIt() {
+  $('img').on('load', function() {
+     $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('g was pressed');
+      return; }
+    if (key.which == 70) {
+      return;}
+    });
+  }
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
