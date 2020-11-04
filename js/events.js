c334cc3a18f4b1a38415339a4a5b32d175dac2d5
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-11-04 20:18:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,29 @@
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').on('click', getIt);
+  
+  $('img').on('load', frameIt);
+  
+  $('input').on('keydown', pressIt);
+  
+  $('form').on('submit', submitIt);
 });
+
+
+function getIt() {
+  alert('Hey!');
+  
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  
+}
+
+function submitIt() {
+  alert('Your form is going to be submitted now.');
+}
