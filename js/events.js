a3dd65ab1438164c219d28105f2ee9262bf35743
Rachid Abu-Hassan <rachid@platform78.com>
2020-11-08 07:26:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,15 +6,23 @@ function getIt() {
 }
 
 function frameIt() {
-
+  $('img').load(function() {
+      $('img').addClass("tasty")
+  })
 }
 
 function pressIt() {
-
+  $(document).keydown(function(key) {
+    if(key.which == 71){
+      alert('g was pressed')
+    }
+  })
 }
 
 function submitIt() {
-
+  $("form").submit(function() {
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
